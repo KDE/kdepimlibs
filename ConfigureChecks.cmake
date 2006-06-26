8dcbe1d8f2fe666c1d00ca775acd23d1009f0d94
Allen Winter <winter@kde.org>
2006-06-26 19:52:31 +0000

    preparing for adding libemailfunctions
    svn path=/trunk/KDE/kdepimlibs/; revision=555249

---
+++ ConfigureChecks.cmake
@@ -0,0 +1,23 @@
+include(CheckIncludeFile)
+include(CheckIncludeFiles)
+include(CheckSymbolExists)
+include(CheckFunctionExists)
+include(CheckLibraryExists)
+include(CheckPrototypeExists)
+include(CheckTypeSize)
+include(MacroBoolTo01)
+# The FindKDE4.cmake module sets _KDE4_PLATFORM_DEFINITIONS with
+# definitions like _GNU_SOURCE that are needed on each platform.
+set(CMAKE_REQUIRED_DEFINITIONS ${_KDE4_PLATFORM_DEFINITIONS})
+
+#now check for dlfcn.h using the cmake supplied CHECK_include_FILE() macro
+# If definitions like -D_GNU_SOURCE are needed for these checks they
+# should be added to _KDE4_PLATFORM_DEFINITIONS when it is originally
+# defined outside this file.  Here we include these definitions in
+# CMAKE_REQUIRED_DEFINITIONS so they will be included in the build of
+# checks below.
+set(CMAKE_REQUIRED_DEFINITIONS ${_KDE4_PLATFORM_DEFINITIONS})
+if (WIN32)
+   set(CMAKE_REQUIRED_LIBRARIES ${KDEWIN32_LIBRARIES} )
+   set(CMAKE_REQUIRED_INCLUDES  ${KDEWIN32_INCLUDES} )
+endif (WIN32)
