f2d5424e1757c8f5edd20bb95f0ba28e5dd40660
Allen Winter <winter@kde.org>
2006-07-04 16:43:15 +0000

    preparing for ktnef.
    svn path=/trunk/KDE/kdepimlibs/; revision=558073

--- config.h.cmake
+++ config.h.cmake
@@ -6,8 +6,17 @@
 /* Define to 1 if you have the <unistd.h> header file. */
 #cmakedefine HAVE_UNISTD_H 1
 
-/* Define to 1 if you have the <stdint.h> header file. */
-#cmakedefine HAVE_STDINT_H 1
+/* Define to 1 if you have the <inttypes.h> header file. */
+#cmakedefine HAVE_INTTYPES_H 1
 
 /* Version number of package */
 #define VERSION "3.9.02"
+
+/* The size of a `unsigned long', as computed by sizeof. */
+#define SIZEOF_UNSIGNED_LONG ${SIZEOF_UNSIGNED_LONG}
+
+/* The size of a `uint64_t', as computed by sizeof. */
+#define SIZEOF_UINT64_T ${SIZEOF_UINT64_T}
+
+/* The size of a `unsigned long long', as computed by sizeof. */
+#define SIZEOF_UNSIGNED_LONG_LONG ${SIZEOF_UNSIGNED_LONG_LONG}
