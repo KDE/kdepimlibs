6f49145caad7c8a9097649abdfaec9549c9adf9f
Allen Winter <winter@kde.org>
2006-11-07 01:33:11 +0000

    indenting, styling, and general cleaning.
    svn path=/trunk/KDE/kdepimlibs/; revision=602870

--- kmime/kmime_charfreq.h
+++ kmime/kmime_charfreq.h
@@ -28,62 +28,65 @@
 
 namespace KMime {
 
-class KMIME_EXPORT CharFreq {
-public:
-  explicit CharFreq( const QByteArray & buf );
-  CharFreq( const char * buf, size_t len );
+class KMIME_EXPORT CharFreq
+{
+  public:
+    explicit CharFreq( const QByteArray & buf );
+    CharFreq( const char * buf, size_t len );
 
-  enum Type { None = 0,
-              EightBitData,
-              Binary = EightBitData,
-	      SevenBitData,
-              EightBitText,
-              SevenBitText };
+    enum Type {
+      None = 0,
+      EightBitData,
+      Binary = EightBitData,
+      SevenBitData,
+      EightBitText,
+      SevenBitText
+    };
 
-  Type type() const;
-  bool isEightBitData() const;
-  bool isEightBitText() const;
-  bool isSevenBitData() const;
-  bool isSevenBitText() const;
+    Type type() const;
+    bool isEightBitData() const;
+    bool isEightBitText() const;
+    bool isSevenBitData() const;
+    bool isSevenBitText() const;
 
-  /**
-   * Returns true if buf has trailing whitespace, i.e. if any line ends
-   * with space (' ') or tab ('\t').
-   */
-  bool hasTrailingWhitespace() const;
+    /**
+     * Returns true if buf has trailing whitespace, i.e. if any line ends
+     * with space (' ') or tab ('\t').
+     */
+    bool hasTrailingWhitespace() const;
 
-  /**
-   * Returns true if buf contains a line that starts with "From ".
-   */
-  bool hasLeadingFrom() const;
+    /**
+     * Returns true if buf contains a line that starts with "From ".
+     */
+    bool hasLeadingFrom() const;
 
-  /**
-   * Returns the percentage of printable characters: printable/total.
-   * If total == 0, the result is undefined.
-   */
-  float printableRatio() const;
+    /**
+     * Returns the percentage of printable characters: printable/total.
+     * If total == 0, the result is undefined.
+     */
+    float printableRatio() const;
 
-  /**
-   * Returns the percentage of control code (CTLs): CTL/total.
-   * If total == 0, the result is undefined.
-   */
-  float controlCodesRatio() const;
+    /**
+     * Returns the percentage of control code (CTLs): CTL/total.
+     * If total == 0, the result is undefined.
+     */
+    float controlCodesRatio() const;
 
-protected:
-  uint NUL;       // count of NUL chars
-  uint CTL;       // count of CTLs (incl. DEL, excl. CR, LF, HT)
-  uint CR, LF;    // count of CRs and LFs
-  uint CRLF;      // count of LFs, preceded by CRs
-  uint printable; // count of printable US-ASCII chars (SPC..~)
-  uint eightBit;  // count of other latin1 chars (those with 8th bit set)
-  uint total;
-  uint lineMin;
-  uint lineMax;
-  bool mTrailingWS;  // does the buffer contain trailing whitespace?
-  bool mLeadingFrom; // does the buffer contain lines starting with "From "?
+  protected:
+    uint NUL;       // count of NUL chars
+    uint CTL;       // count of CTLs (incl. DEL, excl. CR, LF, HT)
+    uint CR, LF;    // count of CRs and LFs
+    uint CRLF;      // count of LFs, preceded by CRs
+    uint printable; // count of printable US-ASCII chars (SPC..~)
+    uint eightBit;  // count of other latin1 chars (those with 8th bit set)
+    uint total;
+    uint lineMin;
+    uint lineMax;
+    bool mTrailingWS;  // does the buffer contain trailing whitespace?
+    bool mLeadingFrom; // does the buffer contain lines starting with "From "?
 
-private:
-  void count( const char * buf, size_t len );
+  private:
+    void count( const char *buf, size_t len );
 };
 
 } // namespace KMime
