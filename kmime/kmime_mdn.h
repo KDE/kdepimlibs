fc10f4ec41e404cc31779eba6d702e2f572859c4
Allen Winter <winter@kde.org>
2006-11-07 00:24:44 +0000

    indenting, style, and general cleaning.
    svn path=/trunk/KDE/kdepimlibs/; revision=602862

--- kmime/kmime_mdn.h
+++ kmime/kmime_mdn.h
@@ -1,7 +1,7 @@
 /*  -*- c++ -*-
     kmime_mdn.h
 
-    This file is part of KMime, the KDE internet mail/usenet news message library.
+    KMime, the KDE internet mail/usenet news message library.
     Copyright (c) 2002 Marc Mutz <mutz@kde.org>
 
     This library is free software; you can redistribute it and/or
@@ -31,161 +31,170 @@ class QByteArray;
 
 namespace KMime {
 
-  namespace MDN {
+namespace MDN {
 
-    /** The following disposition-types are defined:
+/**
+  The following disposition-types are defined:
 
-	@li Displayed    The message has been displayed by the UA to someone
-                         reading the recipient's mailbox.  There is
-			 no guarantee that the content has been
-			 read or understood.
+  @li Displayed    The message has been displayed by the UA to someone
+  reading the recipient's mailbox.  There is
+  no guarantee that the content has been
+  read or understood.
 
-	@li Dispatched   The message has been sent somewhere in some manner
-                         (e.g., printed, faxed, forwarded) without
-			 necessarily having been previously
-			 displayed to the user.  The user may or
-			 may not see the message later.
+  @li Dispatched   The message has been sent somewhere in some manner
+  (e.g., printed, faxed, forwarded) without
+  necessarily having been previously
+  displayed to the user.  The user may or
+  may not see the message later.
 
-        @li Processed    The message has been processed in some manner (i.e.,
-                         by some sort of rules or server) without
-			 being displayed to the user.  The user may
-			 or may not see the message later, or there
-			 may not even be a human user associated
-			 with the mailbox.
+  @li Processed    The message has been processed in some manner (i.e.,
+  by some sort of rules or server) without
+  being displayed to the user.  The user may
+  or may not see the message later, or there
+  may not even be a human user associated
+  with the mailbox.
 
-        @li Deleted      The message has been deleted.  The recipient may or
-                         may not have seen the message.  The
-			 recipient might "undelete" the message at
-			 a later time and read the message.
+  @li Deleted      The message has been deleted.  The recipient may or
+  may not have seen the message.  The
+  recipient might "undelete" the message at
+  a later time and read the message.
 
-        @li Denied       The recipient does not wish the sender to be informed
-                         of the message's disposition.  A UA may
-			 also siliently ignore message disposition
-			 requests in this situation.
+  @li Denied       The recipient does not wish the sender to be informed
+  of the message's disposition.  A UA may
+  also siliently ignore message disposition
+  requests in this situation.
 
-        @li Failed       A failure occurred that prevented the proper
-                         generation of an MDN.  More information
-                         about the cause of the failure may be
-                         contained in a Failure field.  The
-                         "failed" disposition type is not to be
-			 used for the situation in which there is
-			 is some problem in processing the message
-			 other than interpreting the request for an
-			 MDN.  The "processed" or other disposition
-			 type with appropriate disposition
-			 modifiers is to be used in such
-			 situations.
+  @li Failed       A failure occurred that prevented the proper
+  generation of an MDN.  More information
+  about the cause of the failure may be
+  contained in a Failure field.  The
+  "failed" disposition type is not to be
+  used for the situation in which there is
+  is some problem in processing the message
+  other than interpreting the request for an
+  MDN.  The "processed" or other disposition
+  type with appropriate disposition
+  modifiers is to be used in such situations.
 
-       IOW: @p Displayed when - well -displayed
-            @p Dispatched when forwarding unseen ( == new )
-	    @p Processed (maybe) when piping unseen, but probably never used
-	    @p Deleted when deleting unseen
-	    @p Denied on user command
-	    @p Failed on Disposition-Notification-Options containing
-               unknown required options. ( == @em any required options )
-	       @p Failed needs a description in the @p special parameter.
-     **/
-    enum DispositionType {
-      Displayed, Read = Displayed,
-      Deleted,
-      Dispatched, Forwarded = Dispatched,
-      Processed,
-      Denied,
-      Failed
-    };
-    /** The following disposition modifiers are defined:
+  IOW: @p Displayed when - well -displayed
+  @p Dispatched when forwarding unseen ( == new )
+  @p Processed (maybe) when piping unseen, but probably never used
+  @p Deleted when deleting unseen
+  @p Denied on user command
+  @p Failed on Disposition-Notification-Options containing
+  unknown required options. ( == @em any required options )
+  @p Failed needs a description in the @p special parameter.
+*/
+enum DispositionType {
+  Displayed, Read = Displayed,
+  Deleted,
+  Dispatched, Forwarded = Dispatched,
+  Processed,
+  Denied,
+  Failed
+};
 
-       @li Error               An error of some sort occurred
-                               that prevented successful
-			       processing of the message.
-			       Further information is contained
-			       in an Error field.
+/**
+  The following disposition modifiers are defined:
 
-       @li Warning             The message was successfully
-                               processed but some sort of
-			       exceptional condition occurred.
-			       Further information is contained
-			       in a Warning field.
+  @li Error               An error of some sort occurred
+  that prevented successful
+  processing of the message.
+  Further information is contained
+  in an Error field.
 
-       @li Superseded          The message has been
-                               automatically rendered obsolete by
-			       another message received.  The
-			       recipient may still access and
-			       read the message later.
+  @li Warning             The message was successfully
+  processed but some sort of
+  exceptional condition occurred.
+  Further information is contained
+  in a Warning field.
 
-       @li Expired             The message has reached its
-                               expiration date and has been
-			       automatically removed from the
-			       recipient's mailbox.
+  @li Superseded          The message has been
+  automatically rendered obsolete by
+  another message received.  The
+  recipient may still access and
+  read the message later.
 
-       @li MailboxTerminated   The recipient's mailbox has been
-                               terminated and all message in it
-			       automatically removed.
-     **/
-    enum DispositionModifier {
-      Error,
-      Warning,
-      Superseded,
-      Expired,
-      MailboxTerminated
-    };
-\
-    /** The following disposition modes are defined:
+  @li Expired             The message has reached its
+  expiration date and has been
+  automatically removed from the
+  recipient's mailbox.
 
-     @li ManualAction    The disposition described by the disposition type
-                         was a result of an explicit instruction by
-			 the user rather than some sort of
-			 automatically performed action.
+  @li MailboxTerminated   The recipient's mailbox has been
+  terminated and all message in it
+  automatically removed.
+*/
+enum DispositionModifier {
+  Error,
+  Warning,
+  Superseded,
+  Expired,
+  MailboxTerminated
+};
 
-     @li AutomaticAction The disposition described by the disposition
-                         type was a result of an automatic action,
-			 rather than an explicit instruction by the
-			 user for this message.
+/**
+  The following disposition modes are defined:
 
-     IOW: @p ManualAction for user-driven actions,
-          @p AutomanticAction for filtering.
-    **/
-    enum ActionMode {
-      ManualAction,
-      AutomaticAction
-    };
+  @li ManualAction    The disposition described by the disposition type
+  was a result of an explicit instruction by
+  the user rather than some sort of
+  automatically performed action.
 
-    /**
-     @li SentManually      The user explicitly gave permission for this
-                           particular MDN to be sent.
+  @li AutomaticAction The disposition described by the disposition
+  type was a result of an automatic action,
+  rather than an explicit instruction by the
+  user for this message.
 
-     @li SentAutomatically The MDN was sent because the MUA had
-                           previously been configured to do so
-			   automatically.
+  IOW: @p ManualAction for user-driven actions,
+  @p AutomanticAction for filtering.
+*/
+enum ActionMode {
+  ManualAction,
+  AutomaticAction
+};
 
-     IOW: @p SentManually for when we have asked the user
-          @p SentAutomatically when we use the default specified by the user
-     **/
-    enum SendingMode {
-      SentManually,
-      SentAutomatically
-    };
+/**
+  @li SentManually      The user explicitly gave permission for this
+  particular MDN to be sent.
 
-    /** Main function. Generates the content of the
-        message/disposition-notification body part. */
-    KMIME_EXPORT extern QByteArray dispositionNotificationBodyContent( const QString & finalRecipient,
-	       const QByteArray & originalRecipient,
-	       const QByteArray & originalMsgID,
-	       DispositionType disposition,
-	       ActionMode actionMode,
-	       SendingMode sendingMode,
-	       const QList<DispositionModifier> & dispositionModifers
-				   =QList<DispositionModifier>(),
-	       const QString & special=QString() );
+  @li SentAutomatically The MDN was sent because the MUA had
+  previously been configured to do so
+  automatically.
 
-    KMIME_EXPORT extern QString descriptionFor( DispositionType d,
-				   const QList<DispositionModifier> & m
-				   =QList<DispositionModifier>() );
+  IOW: @p SentManually for when we have asked the user
+  @p SentAutomatically when we use the default specified by the user
+*/
+enum SendingMode {
+  SentManually,
+  SentAutomatically
+};
 
-    enum ReturnContent { Nothing, All, HeadersOnly };
+/**
+  Main function. Generates the content of the
+  message/disposition-notification body part. */
+KMIME_EXPORT extern
+QByteArray dispositionNotificationBodyContent( const QString &finalRecipient,
+                                               const QByteArray &originalRecipient,
+                                               const QByteArray &originalMsgID,
+                                               DispositionType disposition,
+                                               ActionMode actionMode,
+                                               SendingMode sendingMode,
+                                               const QList<DispositionModifier> &dispositionModifers
+                                               =QList<DispositionModifier>(),
+                                               const QString & special=QString() );
 
+KMIME_EXPORT extern
+QString descriptionFor( DispositionType d,
+                        const QList<DispositionModifier> &m
+                        =QList<DispositionModifier>() );
 
-  } // namespace MDN
+enum ReturnContent {
+  Nothing,
+  All,
+  HeadersOnly
+};
+
+} // namespace MDN
 
 } // namespace KMime
 
