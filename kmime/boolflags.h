974e07aa913a5a718459bf1516101c76cfa1ef03
Allen Winter <winter@kde.org>
2006-11-07 08:58:09 +0000

    doxygenation.
    svn path=/trunk/KDE/kdepimlibs/; revision=602920

--- kmime/boolflags.h
+++ kmime/boolflags.h
@@ -17,16 +17,25 @@
     the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
     Boston, MA 02110-1301, USA.
 */
+/**
+  @file
+  This file is part of the API for handling MIME data and
+  defines the BoolFlags class.
+
+  @author see AUTHORS file.
+ */
 
 #ifndef __KMIME_BOOLFLAGS_H__
 #define __KMIME_BOOLFLAGS_H__
 
 #include "kmime.h"
 
-/** This class stores boolean values in single bytes.
-    It provides a similar functionality as QBitArray
-    but requires much less memory.
-    We use it to store the flags of an article
+/**
+  @brief
+  Provides a class for storing boolean values in single bytes.
+
+  This class provides functionality similar to QBitArray but requires
+  much less memory.  Only 16-bits (or 2-bytes) can be stored.
 */
 
 namespace KMime {
@@ -34,25 +43,32 @@ namespace KMime {
 class KMIME_EXPORT BoolFlags {
 
   public:
+    /**
+      Constructs an empty 2-byte flag storage.
+    */
     BoolFlags() { clear(); }
+
+    /**
+      Destroys the flag storage.
+    */
     ~BoolFlags() {}
 
     /**
       Sets bit number @p i to the value @p b.
 
-      @param i bit number. Valid values are 0 through 15.
+      @param i is the bit number. Valid values are 0 through 15.
       Higher values will be silently ignored.
-      @param b value to set bit @p i to.
+      @param b is the value to set for bit @p i.
     */
     void set( unsigned int i, bool b=true );
 
     /**
       Get bit number @p i.
 
-      @param i bit number. Valid values are 0 through 15.
+      @param i is the bit number. Valid values are 0 through 15.
       Higher values all return @c false.
-      @return Value of the single bit @p i. Invalid bit
-      numbers return @c false.
+      @return Value of the single bit @p i.
+      Invalid bit numbers return @c false.
     */
     bool get( unsigned int i );
 
@@ -64,7 +80,7 @@ class KMIME_EXPORT BoolFlags {
     /**
       Returns a pointer to the data structure used to store the bits.
     */
-    unsigned char *data()   { return bits; }
+    unsigned char *data() { return bits; }
 
   protected:
     /**
