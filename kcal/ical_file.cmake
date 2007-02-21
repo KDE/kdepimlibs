3894eeb78c9cb50af7a7bb343e468181a6b92fe2
Allen Winter <winter@kde.org>
2007-02-21 16:38:50 +0000

    remove RCS tags from the create header files too
    svn path=/trunk/KDE/kdepimlibs/; revision=635997

--- kcal/ical_file.cmake
+++ kcal/ical_file.cmake
@@ -30,16 +30,12 @@ set(COMBINEDHEADERSICAL
 FILE(WRITE  ${KDE_FILE_H_FILE} "#ifdef __cplusplus\n")
 FILE(APPEND ${KDE_FILE_H_FILE} "extern \"C\" {\n")
 FILE(APPEND ${KDE_FILE_H_FILE} "#endif\n")
-FILE(APPEND ${KDE_FILE_H_FILE} "/*\n")
-FILE(APPEND ${KDE_FILE_H_FILE} " $Id$\n")
-FILE(APPEND ${KDE_FILE_H_FILE} "*/\n")
 
 foreach (_current_FILE ${COMBINEDHEADERSICAL})
    FILE(READ ${_current_FILE} _contents)
    STRING(REGEX REPLACE "#include *\"ical.*\\.h\"" "" _contents "${_contents}")
    STRING(REGEX REPLACE "#include *\"config.*\\.h\"" "" _contents "${_contents}")
    STRING(REGEX REPLACE "#include *\"pvl\\.h\"" "" _contents "${_contents}" )
-   #STRING(REGEX REPLACE " *\$$[Id|Locker]: .+\$$" "" _contents "${_contents}")
    FILE(APPEND ${KDE_FILE_H_FILE} "${_contents}")
 endforeach (_current_FILE)
 
--- kcal/icalss_file.cmake
+++ kcal/icalss_file.cmake
@@ -7,15 +7,11 @@ set(COMBINEDHEADERSICALSS
 FILE(WRITE  ${KDE_FILE_H_FILE} "#ifdef __cplusplus\n")
 FILE(APPEND ${KDE_FILE_H_FILE} "extern \"C\" {\n")
 FILE(APPEND ${KDE_FILE_H_FILE} "#endif\n")
-FILE(APPEND ${KDE_FILE_H_FILE} "/*\n")
-FILE(APPEND ${KDE_FILE_H_FILE} " $Id$\n")
-FILE(APPEND ${KDE_FILE_H_FILE} "*/\n")
 
 foreach (_current_FILE ${COMBINEDHEADERSICALSS})
    FILE(READ ${_current_FILE} _contents)
    STRING(REGEX REPLACE "#include *\"ical.*\\.h\"" "" _contents "${_contents}" )
    STRING(REGEX REPLACE "#include *\"pvl\\.h\"" "" _contents "${_contents}" )
-   #STRING(REGEX REPLACE "\$$(Id|Locker): .+\$$" "" _contents "${_contents}" )
    FILE(APPEND ${KDE_FILE_H_FILE} "${_contents}")
 endforeach (_current_FILE)
 
