419c612ca57eee328609408881a806ebef7f1ae1
Allen Winter <winter@kde.org>
2007-03-23 21:13:52 +0000

    style and indentation
    svn path=/trunk/KDE/kdepimlibs/; revision=645898

--- kblog/blogger_p.cpp
+++ kblog/blogger_p.cpp
@@ -1,22 +1,22 @@
 /*
-    This file is part of the kblog library.
+  This file is part of the kblog library.
 
-    Copyright (c) 2007 Christian Weilbach <christian@whiletaker.homeip.net>
+  Copyright (c) 2007 Christian Weilbach <christian@whiletaker.homeip.net>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
 #include <blogger.h>
@@ -54,63 +54,74 @@ QList<QVariant> APIBlogger::APIBloggerPrivate::defaultArgs( const QString &id )
   return args;
 }
 
-void APIBlogger::APIBloggerPrivate::slotUserInfo( const QList<QVariant> &result, const QVariant &id )
+void APIBlogger::APIBloggerPrivate::slotUserInfo( const QList<QVariant> &result,
+                                                  const QVariant &id )
 {
   // TODO: Implement user authentication
-  kDebug () << "TOP: " << result[ 0 ].typeName() << endl;
-  if( result[ 0 ].type()!=QVariant::Map ){
-    kDebug () << "Could not fetch user information out of the result from the server, not a list." << endl;
-    emit parent->error( ParsingError, i18n("Could not fetch user information out of the result from the server, not a list." ) );
-  }
-  else {
-    const QMap<QString,QVariant> userInfo= result[ 0 ].toMap();
-    const QString nickname = userInfo[ "nickname" ].toString();
-    const QString userid = userInfo[ "userid" ].toString();
-    const QString email = userInfo[ "email" ].toString();
-    kDebug() << "emit userInfoRetrieved( " << nickname << ", " << userid << ", " << email << " )" << endl;
+  kDebug () << "TOP: " << result[0].typeName() << endl;
+  if ( result[0].type() != QVariant::Map ) {
+    kDebug () << "Could not fetch user information out "
+              << "of the result from the server, not a list." << endl;
+    emit parent->error( ParsingError,
+                        i18n("Could not fetch user information out "
+                             "of the result from the server, not a list.") );
+  } else {
+    const QMap<QString,QVariant> userInfo= result[0].toMap();
+    const QString nickname = userInfo["nickname"].toString();
+    const QString userid = userInfo["userid"].toString();
+    const QString email = userInfo["email"].toString();
+    kDebug() << "emit userInfoRetrieved( " << nickname << ", "
+             << userid << ", " << email << " )" << endl;
     // FIXME: What about a BlogUserInfo class/struct?
     emit parent->userInfoRetrieved( nickname, userid, email );
   }
 }
 
-void APIBlogger::APIBloggerPrivate::slotListBlogs( const QList<QVariant> &result, const QVariant &id )
+void APIBlogger::APIBloggerPrivate::slotListBlogs( const QList<QVariant> &result,
+                                                   const QVariant &id )
 {
   kDebug() << "APIBlogger::slotListBlogs" << endl;
-  kDebug () << "TOP: " << result[ 0 ].typeName() << endl;
-  if( result[ 0 ].type()!=QVariant::List ){
-    kDebug () << "Could not fetch blogs out of the result from the server, not a list." << endl;
-    emit parent->error( ParsingError, i18n("Could not blogs Posting out of the result from the server, not a list." ) );
-  }
-  else {
-    const QList<QVariant> posts = result[ 0 ].toList();
+  kDebug () << "TOP: " << result[0].typeName() << endl;
+  if ( result[0].type() != QVariant::List ) {
+    kDebug () << "Could not fetch blogs out of the result from the server, "
+              << "not a list." << endl;
+    emit parent->error( ParsingError,
+                        i18n("Could not blogs Posting out of the result "
+                             "from the server, not a list.") );
+  } else {
+    const QList<QVariant> posts = result[0].toList();
     QList<QVariant>::ConstIterator it = posts.begin();
     QList<QVariant>::ConstIterator end = posts.end();
     for ( ; it != end; ++it ) {
       kDebug () << "MIDDLE: " << ( *it ).typeName() << endl;
       const QMap<QString, QVariant> postInfo = ( *it ).toMap();
 
-      const QString id( postInfo[ "blogid" ].toString() );
-      const QString name( postInfo[ "blogName" ].toString() );
-      const QString url( postInfo[ "url" ].toString() );
+      const QString id( postInfo["blogid"].toString() );
+      const QString name( postInfo["blogName"].toString() );
+      const QString url( postInfo["url"].toString() );
 
       if ( !id.isEmpty() && !name.isEmpty() ) {
         emit parent->blogInfoRetrieved( id, name );
-        kDebug()<< "Emitting blogInfoRetrieved( id=" << id << ", name=" << name << "); " << endl;
+        kDebug() << "Emitting blogInfoRetrieved( id=" << id
+                 << ", name=" << name << "); " << endl;
       }
     }
   }
 }
 
-void APIBlogger::APIBloggerPrivate::slotListPostings( const QList<QVariant> &result, const QVariant &id )
+void APIBlogger::APIBloggerPrivate::slotListPostings( const QList<QVariant> &result,
+                                                      const QVariant &id )
 {
-  kDebug()<<"APIBlogger::slotListPostings"<<endl;
-  kDebug () << "TOP: " << result[ 0 ].typeName() << endl;
-  if( result[ 0 ].type()!=QVariant::List ){
-    kDebug () << "Could not fetch list of postings out of the result from the server, not a list." << endl;
-    emit parent->error( ParsingError, i18n("Could not fetch list of postings out of the result from the server, not a list." ) );
-  }
-  else {
-    const QList<QVariant> postReceived = result[ 0 ].toList();
+  kDebug() << "APIBlogger::slotListPostings" << endl;
+  kDebug () << "TOP: " << result[0].typeName() << endl;
+  if ( result[0].type() != QVariant::List ) {
+    kDebug () << "Could not fetch list of postings out of the "
+              << "result from the server, not a list." << endl;
+    emit parent->error( ParsingError,
+                        i18n("Could not fetch list of postings out of the "
+                             "result from the server, not a list.") );
+  } else {
+    const QList<QVariant> postReceived = result[0].toList();
     QList<QVariant>::ConstIterator it = postReceived.begin();
     QList<QVariant>::ConstIterator end = postReceived.end();
     for ( ; it != end; ++it ) {
@@ -118,7 +129,8 @@ void APIBlogger::APIBloggerPrivate::slotListPostings( const QList<QVariant> &res
       kDebug () << "MIDDLE: " << ( *it ).typeName() << endl;
       const QMap<QString, QVariant> postInfo = ( *it ).toMap();
       if ( readPostingFromMap( &posting, postInfo ) ) {
-        kDebug() << "Emitting listedPosting( posting.postingId()=" <<posting.postingId() << "); " << endl;
+        kDebug() << "Emitting listedPosting( posting.postingId()="
+                 << posting.postingId() << "); " << endl;
         emit parent->listedPosting( posting ); // KUrl( posting.postingId() ) );
       } else {
         kDebug() << "d->readPostingFromMap failed! " << endl;
@@ -130,23 +142,28 @@ void APIBlogger::APIBloggerPrivate::slotListPostings( const QList<QVariant> &res
   emit parent->listPostingsFinished();
 }
 
-void APIBlogger::APIBloggerPrivate::slotFetchPosting( const QList<QVariant> &result, const QVariant &id )
+void APIBlogger::APIBloggerPrivate::slotFetchPosting( const QList<QVariant> &result,
+                                                      const QVariant &id )
 {
-  kDebug()<<"APIBlogger::slotFetchPosting"<<endl;
-  //array of structs containing ISO.8601 dateCreated, String userid, String postid, String content;
+  kDebug() << "APIBlogger::slotFetchPosting" << endl;
+  //array of structs containing ISO.8601
+  // dateCreated, String userid, String postid, String content;
   // TODO: Time zone for the dateCreated!
-  kDebug () << "TOP: " << result[ 0 ].typeName() << endl;
-  if( result[ 0 ].type()!=QVariant::Map ){
-    kDebug () << "Could not fetch posting out of the result from the server." << endl;
-    emit parent->error( ParsingError, i18n("Could not fetch posting out of the result from the server." ) );
-  }
-  else {
-//     const QList<QVariant> postReceived = result[ 0 ].toList();
+  kDebug () << "TOP: " << result[0].typeName() << endl;
+  if ( result[0].type() != QVariant::Map ) {
+    kDebug () << "Could not fetch posting out of the result from "
+              << "the server." << endl;
+    emit parent->error( ParsingError,
+                        i18n("Could not fetch posting out of the result from "
+                             "the server.") );
+  } else {
+//     const QList<QVariant> postReceived = result[0].toList();
 //     QList<QVariant>::ConstIterator it = postReceived.begin();
     BlogPosting posting;
-    const QMap<QString, QVariant> postInfo = result[ 0 ].toMap();
+    const QMap<QString, QVariant> postInfo = result[0].toMap();
     if ( readPostingFromMap( &posting, postInfo ) ) {
-      kDebug() << "Emitting fetchedPosting( posting.postingId()=" <<posting.postingId() << "); " << endl;
+      kDebug() << "Emitting fetchedPosting( posting.postingId()="
+               << posting.postingId() << "); " << endl;
       emit parent->fetchedPosting( posting ); // KUrl( posting.posingtId() ) );
     } else {
       kDebug() << "d->readPostingFromMap failed! " << endl;
@@ -155,64 +172,76 @@ void APIBlogger::APIBloggerPrivate::slotFetchPosting( const QList<QVariant> &res
   }
 }
 
-void APIBlogger::APIBloggerPrivate::slotCreatePosting( const QList<QVariant> &result, const QVariant &id )
+void APIBlogger::APIBloggerPrivate::slotCreatePosting( const QList<QVariant> &result,
+                                                       const QVariant &id )
 {
-  kDebug()<<"APIBlogger::slotCreatePosting"<<endl;
-  //array of structs containing ISO.8601 dateCreated, String userid, String postid, String content;
+  kDebug() << "APIBlogger::slotCreatePosting" << endl;
+  //array of structs containing ISO.8601
+  // dateCreated, String userid, String postid, String content;
   // TODO: Time zone for the dateCreated!
-  kDebug () << "TOP: " << result[ 0 ].typeName() << endl;
-  if( result[ 0 ].type()!=QVariant::Int ){
+  kDebug () << "TOP: " << result[0].typeName() << endl;
+  if ( result[0].type() != QVariant::Int ) {
     kDebug () << "Could not read the postingId, not an integer." << endl;
-    emit parent->error( ParsingError, i18n( "Could not read the postingId, not an integer." ) );
-  }
-  else {
-    emit parent->createdPosting( QString().setNum( result[ 0 ].toInt() ) );
-    kDebug() << "emitting createdPosting( " << result[ 0 ].toInt() << " )" << endl;
+    emit parent->error( ParsingError,
+                        i18n("Could not read the postingId, not an integer.") );
+  } else {
+    emit parent->createdPosting( QString().setNum( result[0].toInt() ) );
+    kDebug() << "emitting createdPosting( " << result[0].toInt()
+             << " )" << endl;
   }
 }
 
-void APIBlogger::APIBloggerPrivate::slotModifyPosting( const QList<QVariant> &result, const QVariant &id )
+void APIBlogger::APIBloggerPrivate::slotModifyPosting( const QList<QVariant> &result,
+                                                       const QVariant &id )
 {
-  kDebug()<<"APIBlogger::slotModifyPosting"<<endl;
-  //array of structs containing ISO.8601 dateCreated, String userid, String postid, String content;
+  kDebug() << "APIBlogger::slotModifyPosting" << endl;
+  //array of structs containing ISO.8601
+  // dateCreated, String userid, String postid, String content;
   // TODO: Time zone for the dateCreated!
-  kDebug () << "TOP: " << result[ 0 ].typeName() << endl;
-  if( result[ 0 ].type()!=QVariant::Bool ){
+  kDebug () << "TOP: " << result[0].typeName() << endl;
+  if ( result[0].type() != QVariant::Bool ) {
     kDebug () << "Could not read the result, not a boolean." << endl;
-    emit parent->error( ParsingError, i18n( "Could not read the result, not a boolean." ) );
-  }
-  else {
-    emit parent->modifiedPosting( result[ 0 ].toBool() );
-    kDebug() << "emitting modifiedPosting( " << result[ 0 ].toBool() << " )" << endl;
+    emit parent->error( ParsingError,
+                        i18n( "Could not read the result, not a boolean.") );
+  } else {
+    emit parent->modifiedPosting( result[0].toBool() );
+    kDebug() << "emitting modifiedPosting( " << result[0].toBool()
+             << " )" << endl;
   }
 }
 
-void APIBlogger::APIBloggerPrivate::faultSlot( int number, const QString& errorString, const QVariant& id )
+void APIBlogger::APIBloggerPrivate::faultSlot( int number,
+                                               const QString &errorString,
+                                               const QVariant &id )
 {
   emit parent->error( XmlRpc, errorString );
 }
 
-bool APIBlogger::APIBloggerPrivate::readPostingFromMap( BlogPosting *post, const QMap<QString, QVariant> &postInfo )
+bool APIBlogger::APIBloggerPrivate::readPostingFromMap( BlogPosting *post,
+                                                        const QMap<QString, QVariant> &postInfo )
 {
   // FIXME: integrate error handling
-  if ( !post ) return false;
+  if ( !post ) {
+    return false;
+  }
   QStringList mapkeys = postInfo.keys();
   kDebug() << endl << "Keys: " << mapkeys.join(", ") << endl << endl;
 
-  KDateTime dt( postInfo[ "dateCreated" ].toDateTime(), KDateTime::Spec::UTC );
+  KDateTime dt( postInfo["dateCreated"].toDateTime(), KDateTime::Spec::UTC );
   if ( dt.isValid() && !dt.isNull() ) {
     post->setCreationDateTime( dt );
   }
-  dt = KDateTime ( postInfo[ "lastModified" ].toDateTime(), KDateTime::Spec::UTC );
+  dt = KDateTime ( postInfo["lastModified"].toDateTime(), KDateTime::Spec::UTC );
   if ( dt.isValid() && !dt.isNull() ) {
     post->setModificationDateTime( dt );
   }
-//  post->setUserId( postInfo[ "userid" ].toString() ); TODO remove if sure that not needed
-  post->setPostingId( postInfo[ "postid" ].toString() );
+  //TODO remove if sure that not needed
+  //post->setUserId( postInfo["userid"].toString() );
+  post->setPostingId( postInfo["postid"].toString() );
 
-  QString title( postInfo[ "title" ].toString() );
-  QString description( postInfo[ "description" ].toString() );
-  QString contents( postInfo[ "content" ].toString() );
+  QString title( postInfo["title"].toString() );
+  QString description( postInfo["description"].toString() );
+  QString contents( postInfo["content"].toString() );
   QString category;
 
   post->setTitle( title );
--- kblog/blogger_p.h
+++ kblog/blogger_p.h
@@ -1,22 +1,22 @@
 /*
-    This file is part of the kblog library.
+  This file is part of the kblog library.
 
-    Copyright (c) 2007 Christian Weilbach <christian@whiletaker.homeip.net>
+  Copyright (c) 2007 Christian Weilbach <christian@whiletaker.homeip.net>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
 #ifndef BLOGGER_P_H
@@ -46,8 +46,9 @@ class APIBlogger::APIBloggerPrivate : public QObject
     void slotFetchPosting( const QList<QVariant> &result, const QVariant &id );
     void slotCreatePosting( const QList<QVariant> &result, const QVariant &id );
     void slotModifyPosting( const QList<QVariant> &result, const QVariant &id );
-    void faultSlot( int, const QString&, const QVariant& );
-    bool readPostingFromMap( BlogPosting *post, const QMap<QString, QVariant> &postInfo );
+    void faultSlot( int number, const QString &errorString, const QVariant &id );
+    bool readPostingFromMap( BlogPosting *post,
+                             const QMap<QString, QVariant> &postInfo );
 };
 
 #endif
