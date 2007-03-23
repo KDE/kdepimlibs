4d2da977cca4329fe328a76670317fb20026fe6f
Allen Winter <winter@kde.org>
2007-03-23 20:16:46 +0000

    style and indentation.
    svn path=/trunk/KDE/kdepimlibs/; revision=645877

--- kblog/blogger.cpp
+++ kblog/blogger.cpp
@@ -1,23 +1,23 @@
 /*
-    This file is part of the kblog library.
+  This file is part of the kblog library.
 
-    Copyright (c) 2004 Reinhold Kainhofer <reinhold@kainhofer.com>
-    Copyright (c) 2006 Christian Weilbach <christian@whiletaker.homeip.net>
+  Copyright (c) 2004 Reinhold Kainhofer <reinhold@kainhofer.com>
+  Copyright (c) 2006 Christian Weilbach <christian@whiletaker.homeip.net>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
 #include <blogger.h>
@@ -31,9 +31,8 @@
 
 using namespace KBlog;
 
-
-APIBlogger::APIBlogger( const KUrl &server, QObject *parent, const char *name ) : APIBlog( server, parent, name ), 
-d( new APIBloggerPrivate )
+APIBlogger::APIBlogger( const KUrl &server, QObject *parent, const char *name )
+  : APIBlog( server, parent, name ), d( new APIBloggerPrivate )
 {
   d->parent = this;
   setUrl( server );
@@ -56,18 +55,20 @@ void APIBlogger::userInfo()
 {
   kDebug() << "read user info..." << endl;
   QList<QVariant> args( d->defaultArgs() );
-  d->mXmlRpcClient->call( "blogger.getUserInfo", args, 
-                          d, SLOT( slotUserInfo( const QList<QVariant>&, const QVariant& ) ), 
-			  d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call(
+    "blogger.getUserInfo", args,
+    d, SLOT( slotUserInfo( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
 void APIBlogger::listBlogs()
 {
   kDebug() << "Fetch List of Blogs..." << endl;
   QList<QVariant> args( d->defaultArgs() );
-  d->mXmlRpcClient->call( "blogger.getUsersBlogs", args, 
-                          d, SLOT( slotListBlogs( const QList<QVariant>&, const QVariant& ) ), 
-			  d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call(
+    "blogger.getUsersBlogs", args,
+    d, SLOT( slotListBlogs( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
 void APIBlogger::listPostings()
@@ -75,13 +76,16 @@ void APIBlogger::listPostings()
   kDebug() << "Fetching List of Posts..." << endl;
   QList<QVariant> args( d->defaultArgs( blogId() ) );
   args << QVariant( downloadCount() );
-  d->mXmlRpcClient->call( "blogger.getRecentPosts", args, 
-                          d, SLOT( slotListPostings( const QList<QVariant>&, const QVariant& ) ), 
-			  d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call(
+    "blogger.getRecentPosts", args,
+    d, SLOT( slotListPostings( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
-void APIBlogger::listCategories(){
-  emit error( NotSupported, i18n( "Categories are not supported in Blogger API 1.0." ) );
+void APIBlogger::listCategories()
+{
+  emit error( NotSupported,
+              i18n( "Categories are not supported in Blogger API 1.0." ) );
   kDebug() << "Categories are not supported in Blogger API 1.0." << endl;
 }
 
@@ -89,43 +93,51 @@ void APIBlogger::fetchPosting( const QString &postingId )
 {
   kDebug() << "Fetching Posting with url " << postingId << endl;
   QList<QVariant> args( d->defaultArgs( postingId ) );
-  d->mXmlRpcClient->call( "blogger.getPost", args, 
-                    d, SLOT( slotFetchPosting( const QList<QVariant>&, const QVariant& ) ), 
-		    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call(
+    "blogger.getPost", args,
+    d, SLOT( slotFetchPosting( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
-void APIBlogger::modifyPosting( KBlog::BlogPosting* posting )
+void APIBlogger::modifyPosting( KBlog::BlogPosting *posting )
 {
   if ( !posting ) {
     kDebug() << "APIBlogger::modifyPosting: posting is null pointer" << endl;
     return;
   }
-    kDebug() << "Uploading Posting with postingId " << posting->postingId() << endl;
-    QList<QVariant> args( d->defaultArgs( posting->postingId() ) );
-    args << QVariant( posting->content() );
-    args << QVariant( posting->publish() );
-    d->mXmlRpcClient->call( "blogger.editPost", args, 
-                            d, SLOT( slotModifyPosting( const QList<QVariant>&, const QVariant& ) ), 
-			    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
+  kDebug() << "Uploading Posting with postingId "
+           << posting->postingId() << endl;
+
+  QList<QVariant> args( d->defaultArgs( posting->postingId() ) );
+  args << QVariant( posting->content() );
+  args << QVariant( posting->publish() );
+  d->mXmlRpcClient->call(
+    "blogger.editPost", args,
+    d, SLOT( slotModifyPosting( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
-void APIBlogger::createPosting( KBlog::BlogPosting* posting )
+void APIBlogger::createPosting( KBlog::BlogPosting *posting )
 {
   if ( !posting ) {
     kDebug() << "APIBlogger::createPosting: posting is null pointer" << endl;
     return;
   }
-    kDebug() << "Creating new Posting with blogid " << blogId() << endl;
-    QList<QVariant> args( d->defaultArgs( blogId() ) );
-    args << QVariant( posting->content() );
-    args << QVariant( posting->publish() );
-    d->mXmlRpcClient->call( "blogger.newPost", args, 
-                            d, SLOT( slotCreatePosting( const QList<QVariant>&, const QVariant& ) ), 
-			    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
+
+  kDebug() << "Creating new Posting with blogid " << blogId() << endl;
+  QList<QVariant> args( d->defaultArgs( blogId() ) );
+  args << QVariant( posting->content() );
+  args << QVariant( posting->publish() );
+  d->mXmlRpcClient->call(
+    "blogger.newPost", args,
+    d, SLOT( slotCreatePosting( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
-void APIBlogger::createMedia( KBlog::BlogMedia* media ){
-  emit error( NotSupported, i18n( "Media upload not available in Blogger API 1.0." ) );
+void APIBlogger::createMedia( KBlog::BlogMedia *media )
+{
+  emit error( NotSupported,
+              i18n( "Media upload not available in Blogger API 1.0." ) );
   kDebug() << "Media upload not available in Blogger API 1.0." << endl;
 }
 
@@ -134,9 +146,10 @@ void APIBlogger::removePosting( const QString &postingId )
   kDebug() << "APIBlogger::removePosting: postingId=" << postingId << endl;
   QList<QVariant> args( d->defaultArgs( postingId ) );
   args << QVariant( /*publish=*/true );
-  d->mXmlRpcClient->call( "blogger.deletePost", args, 
-                          d, SLOT( slotCreateMedia( QList<QVariant> &result, QVariant &id ) ), 
-			  d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call(
+    "blogger.deletePost", args,
+    d, SLOT( slotCreateMedia( QList<QVariant> &result, QVariant &id ) ),
+    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
 #include "blogger.moc"
--- kblog/blogger.h
+++ kblog/blogger.h
@@ -1,23 +1,23 @@
 /*
-    This file is part of the kblog library.
+  This file is part of the kblog library.
 
-    Copyright (c) 2004 Reinhold Kainhofer <reinhold@kainhofer.com>
-    Copyright (c) 2006 Christian Weilbach <christian@whiletaker.homeip.net>
+  Copyright (c) 2004 Reinhold Kainhofer <reinhold@kainhofer.com>
+  Copyright (c) 2006 Christian Weilbach <christian@whiletaker.homeip.net>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 #ifndef API_BLOGGER_H
 #define API_BLOGGER_H
@@ -40,113 +40,119 @@
   @author Christian Weilbach \<christian\@whiletaker.homeip.net\>
 
   \par Maintainer: Christian Weilbach \<christian\@whiletaker.homeip.net\>
- */
+*/
 
 namespace KBlog {
 /**
-  @brief
-  A class that can be used for access to Blogger API 1.0 blogs. Almost every blog server
-  supports Blogger API 1.0 . Compared to MetaWeblog API it is not as functional and is obsolete compared to Blogger API 2.0 which uses Atom API instead of Xml-Rpc.
+   @brief
+   A class that can be used for access to Blogger API 1.0 blogs.
+   Almost every blog server supports Blogger API 1.0. Compared to
+   MetaWeblog API it is not as functional and is obsolete compared to
+   Blogger API 2.0 which uses Atom API instead of Xml-Rpc.
 
-  @code
-  APIBlog* myblog = new APIBlogger( "http://example.com/xmlrpc/gateway.php" );
-  KBlog::BlogPosting *post = new BlogPosting();
-  post->setUserId( "some_user_id" );
-  post->setTitle( "This is the title." );
-  post->setContent( "Here is some the content..." );
-  myblog->createPosting( posting );
-  @endcode
+   @code
+   APIBlog* myblog = new APIBlogger( "http://example.com/xmlrpc/gateway.php" );
+   KBlog::BlogPosting *post = new BlogPosting();
+   post->setUserId( "some_user_id" );
+   post->setTitle( "This is the title." );
+   post->setContent( "Here is some the content..." );
+   myblog->createPosting( posting );
+   @endcode
 
-  @author Christian Weilbach \<christian\@whiletaker.homeip.net\>
-  @author Reinhold Kainhofer \<reinhold\@kainhofer.com\>
- */
+   @author Christian Weilbach \<christian\@whiletaker.homeip.net\>
+   @author Reinhold Kainhofer \<reinhold\@kainhofer.com\>
+*/
 class KBLOG_EXPORT APIBlogger : public APIBlog
 {
   Q_OBJECT
   public:
     /**
-         Create an object for Blogger API 1.0
+      Create an object for Blogger API 1.0
 
-         @param server is the url for the xmlrpc gateway.
+      @param server is the url for the xmlrpc gateway.
     */
     explicit APIBlogger( const KUrl &server, QObject *parent = 0,
-                        const char *name = 0 );
+                         const char *name = 0 );
     virtual ~APIBlogger();
 
     /**
-        Returns the API of the inherited object.
+      Returns the API of the inherited object.
     */
     QString interfaceName() const { return "Blogger API 1.0"; }
     void setUrl( const KUrl &server );
 
     /**
-        Get information about the user from the blog.
-	@see void userInfoRetrieved( const QString &nickname, const QString &userid, const QString &email )
+      Get information about the user from the blog.
+      @see void userInfoRetrieved( const QString &, const QString &, const QString & )
     */
     virtual void userInfo();
 
     /**
-        List the blogs available for this authentication on the server.
-	@see void blogInfoRetrieved( const QString &id, const QString &name )
+      List the blogs available for this authentication on the server.
+      @see void blogInfoRetrieved( const QString &id, const QString &name )
     */
     virtual void listBlogs();
 
     /**
-        List recent postings on the server..
-	@see     void listedPosting( KBlog::BlogPosting &posting )
-        @see     void fetchedPosting( KBlog::BlogPosting &posting )
-        @see     void listPostingsFinished()
+      List recent postings on the server..
+      @see     void listedPosting( KBlog::BlogPosting &posting )
+      @see     void fetchedPosting( KBlog::BlogPosting &posting )
+      @see     void listPostingsFinished()
     */
     virtual void listPostings();
 
     /**
-        List the categories of the blog. Note: This is not supported on the server side.
-	@see  void categoryInfoRetrieved( const QString &name, const QString &description )
-        @see  void listCategoriesFinished()
+      List the categories of the blog.
+      Note: This is not supported on the server side.
+
+      @see  void categoryInfoRetrieved( const QString &, const QString & )
+      @see  void listCategoriesFinished()
     */
     virtual void listCategories();
 
     /**
-        Fetch the Posting with postingId.
-        @param postingId is the id of the posting on the server.
+      Fetch the Posting with postingId.
+      @param postingId is the id of the posting on the server.
 
-        @see  void fetchedPosting( KBlog::BlogPosting &posting )
+      @see  void fetchedPosting( KBlog::BlogPosting &posting )
     */
     virtual void fetchPosting( const QString &postingId );
 
     /**
-        Modify a posting on server.
+      Modify a posting on server.
 
-        @param posting is used to send the modified posting including the correct postingId from it to the server.
+      @param posting is used to send the modified posting including
+      the correct postingId from it to the server.
     */
     virtual void modifyPosting( KBlog::BlogPosting *posting );
 
     /**
-        Create a new posting on server.
+      Create a new posting on server.
 
-        @param posting is send to the server.
+      @param posting is send to the server.
     */
     virtual void createPosting( KBlog::BlogPosting *posting );
 
     /**
-        Create a new media object, e.g. picture, on server. Note: This is not supported on the servers side.
+      Create a new media object, e.g. picture, on server.
+      Note: This is not supported on the server side.
 
-        @param media is send to the server.
+      @param media is send to the server.
     */
     virtual void createMedia( KBlog::BlogMedia *media );
 
     /**
-        Remove a posting from the server.
+      Remove a posting from the server.
 
-        @param postingId is the id of the posting to remove.
+      @param postingId is the id of the posting to remove.
 
-        @see void removePosting( KBlog::BlogPosting *posting )
+      @see void removePosting( KBlog::BlogPosting *posting )
     */
     virtual void removePosting( const QString &postingId );
 
-private:
+  private:
     class APIBloggerPrivate;
-    APIBloggerPrivate* const d;
+    APIBloggerPrivate *const d;
 };
 
 }
