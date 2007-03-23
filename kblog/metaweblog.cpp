90d728db880a1f66c759c93cf3358b23f173181b
Allen Winter <winter@kde.org>
2007-03-23 21:27:27 +0000

    more styling and indenting
    svn path=/trunk/KDE/kdepimlibs/; revision=645910

--- kblog/metaweblog.cpp
+++ kblog/metaweblog.cpp
@@ -1,23 +1,23 @@
 /*
-    This file is part of the kblog library.
+  This file is part of the kblog library.
 
-    Copyright (c) 2004 Reinhold Kainhofer <reinhold@kainhofer.com>
-    Copyright (c) 2006 Christian Weilbach <christian@whiletaker.homeip.net>
+  Copyright (c) 2004 Reinhold Kainhofer <reinhold@kainhofer.com>
+  Copyright (c) 2006 Christian Weilbach <christian@whiletaker.homeip.net>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
 #include <metaweblog.h>
@@ -31,9 +31,9 @@
 
 using namespace KBlog;
 
-
-APIMetaWeblog::APIMetaWeblog( const KUrl &server, QObject *parent, const char *name ) : APIBlog( server, parent, name ), 
-d( new APIMetaWeblogPrivate )
+APIMetaWeblog::APIMetaWeblog( const KUrl &server, QObject *parent,
+                              const char *name )
+  : APIBlog( server, parent, name ), d( new APIMetaWeblogPrivate )
 {
   d->parent = this;
   setUrl( server );
@@ -54,14 +54,20 @@ void APIMetaWeblog::setUrl( const KUrl &server )
 
 void APIMetaWeblog::userInfo()
 {
-  kDebug() << "Fetching user information is not available in MetaWeblog API." << endl;
-  emit error( NotSupported, i18n( "Fetching user information is not available in MetaWeblog API." ) );
+  kDebug() << "Fetching user information is not available in MetaWeblog API."
+           << endl;
+  emit error( NotSupported,
+              i18n("Fetching user information is not available in "
+                   "MetaWeblog API.") );
 }
 
 void APIMetaWeblog::listBlogs()
 {
-  kDebug() << "Fetching user's blogs is not available in MetaWeblog API." << endl;
-  emit error( NotSupported, i18n( "Fetching user's blogs is not available in MetaWeblog API." ) );
+  kDebug() << "Fetching user's blogs is not available in MetaWeblog API."
+           << endl;
+  emit error( NotSupported,
+              i18n("Fetching user's blogs is not available in "
+                   "MetaWeblog API.") );
 }
 
 void APIMetaWeblog::listPostings()
@@ -69,33 +75,37 @@ void APIMetaWeblog::listPostings()
   kDebug() << "Fetching List of Posts..." << endl;
   QList<QVariant> args( d->defaultArgs( blogId() ) );
   args << QVariant( downloadCount() );
-  d->mXmlRpcClient->call( "metaWeblog.getRecentPosts", args, 
-                          d, SLOT( slotListPostings( const QList<QVariant>&, const QVariant& ) ), 
-			  d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call(
+    "metaWeblog.getRecentPosts", args,
+    d, SLOT( slotListPostings( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
-void APIMetaWeblog::listCategories(){
+void APIMetaWeblog::listCategories()
+{
   kDebug() << "Fetching List of Categories..." << endl;
   QList<QVariant> args( d->defaultArgs( blogId() ) );
-  d->mXmlRpcClient->call( "metaWeblog.getCategories", args, 
-                          d, SLOT( slotListCategories( const QList<QVariant>&, const QVariant& ) ), 
-			  d, SLOT ( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call(
+    "metaWeblog.getCategories", args,
+    d, SLOT( slotListCategories( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT ( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
 void APIMetaWeblog::fetchPosting( const QString &postingId )
 {
   kDebug() << "Fetching Posting with url " << postingId << endl;
   QList<QVariant> args( d->defaultArgs( postingId ) );
-  d->mXmlRpcClient->call( "metaWeblog.getPost", args, 
-                    d, SLOT( slotFetchPosting( const QList<QVariant>&, const QVariant& ) ), 
-		    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call(
+    "metaWeblog.getPost", args,
+    d, SLOT( slotFetchPosting( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
-void APIMetaWeblog::modifyPosting( KBlog::BlogPosting* posting )
+void APIMetaWeblog::modifyPosting( KBlog::BlogPosting *posting )
 {
   if ( !posting ) {
     kDebug() << "APIMetaWeblog::modifyPosting: posting null pointer" << endl;
-    emit error ( Other, i18n( "Posting is a null pointer." ) );
+    emit error ( Other, i18n("Posting is a null pointer.") );
     return;
   }
   kDebug() << "Uploading Posting with postId " << posting->postingId() << endl;
@@ -104,22 +114,23 @@ void APIMetaWeblog::modifyPosting( KBlog::BlogPosting* posting )
   QMap<QString, QVariant> map;
   QList<QVariant> list;
   list.append( QString( posting->category() ) );
-  map["categories"]=list;
-  map["description"]=posting->content();
-  map["title"]=posting->title();
-  map["lastModified"]=posting->modificationDateTime().toUtc().dateTime();
+  map["categories"] = list;
+  map["description"] = posting->content();
+  map["title"] = posting->title();
+  map["lastModified"] = posting->modificationDateTime().toUtc().dateTime();
   args << map;
   args << QVariant( posting->publish() );
-  d->mXmlRpcClient->call( "metaWeblog.editPost", args, 
-                          d, SLOT( slotModifyPosting( const QList<QVariant>&, const QVariant& ) ), 
-			  d, SLOT ( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call(
+    "metaWeblog.editPost", args,
+    d, SLOT( slotModifyPosting( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT ( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
-void APIMetaWeblog::createPosting( KBlog::BlogPosting* posting )
+void APIMetaWeblog::createPosting( KBlog::BlogPosting *posting )
 {
   if ( !posting ) {
     kDebug() << "APIMetaWeblog::createPosting: posting null pointer" << endl;
-    emit error ( Other, i18n( "Posting is a null pointer." ) );
+    emit error ( Other, i18n("Posting is a null pointer.") );
     return;
   }
   kDebug() << "Creating new Posting with blogId " << blogId() << endl;
@@ -127,29 +138,32 @@ void APIMetaWeblog::createPosting( KBlog::BlogPosting* posting )
   QMap<QString, QVariant> map;
   QList<QVariant> list;
   list.append( QString( posting->category() ) );
-  map["categories"]=list;
-  map["description"]=posting->content();
-  map["title"]=posting->title();
-  map["dateCreated"]=posting->creationDateTime().toUtc().dateTime();
+  map["categories"] = list;
+  map["description"] = posting->content();
+  map["title"] = posting->title();
+  map["dateCreated"] = posting->creationDateTime().toUtc().dateTime();
   args << map;
   args << QVariant( posting->publish() );
-  d->mXmlRpcClient->call( "metaWeblog.newPost", args, 
-                          d, SLOT( slotCreatePosting( const QList<QVariant>&, const QVariant& ) ), 
-			  d, SLOT ( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call (
+    "metaWeblog.newPost", args,
+    d, SLOT( slotCreatePosting( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT ( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
-void APIMetaWeblog::createMedia( KBlog::BlogMedia* media ){
+void APIMetaWeblog::createMedia( KBlog::BlogMedia *media )
+{
   kDebug() << "APIMetaWeblog::createMedia: name="<< media->name() << endl;
   QList<QVariant> args( d->defaultArgs( blogId() ) );
   QMap<QString, QVariant> map;
   QList<QVariant> list;
-  map["name"]=media->name();
-  map["type"]=media->mimetype();
-  map["bits"]=media->data();
+  map["name"] = media->name();
+  map["type"] = media->mimetype();
+  map["bits"] = media->data();
   args << map;
-  d->mXmlRpcClient->call( "metaWeblog.newMediaObject", args, 
-                          d, SLOT( slotCreateMedia( const QList<QVariant>&, const QVariant& ) ), 
-			  d, SLOT ( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call(
+    "metaWeblog.newMediaObject", args,
+    d, SLOT( slotCreateMedia( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT ( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
 void APIMetaWeblog::removePosting( const QString &postingId )
@@ -157,9 +171,10 @@ void APIMetaWeblog::removePosting( const QString &postingId )
   kDebug() << "APIMetaWeblog::removePosting: postingId=" << postingId << endl;
   QList<QVariant> args( d->defaultArgs( postingId ) );
   args << QVariant( /*publish=*/true );
-  d->mXmlRpcClient->call( "metaWeblog.deletePost", args, 
-                          d, SLOT( slotCreateMedia( const QList<QVariant>&, const QVariant& ) ), 
-			  d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
+  d->mXmlRpcClient->call(
+    "metaWeblog.deletePost", args,
+    d, SLOT( slotCreateMedia( const QList<QVariant>&, const QVariant& ) ),
+    d, SLOT( faultSlot( int, const QString&, const QVariant& ) ) );
 }
 
 #include "metaweblog.moc"
--- kblog/metaweblog.h
+++ kblog/metaweblog.h
@@ -1,23 +1,23 @@
 /*
-    This file is part of the kblog library.
+  This file is part of the kblog library.
 
-    Copyright (c) 2004 Reinhold Kainhofer <reinhold@kainhofer.com>
-    Copyright (c) 2006 Christian Weilbach <christian@whiletaker.homeip.net>
+  Copyright (c) 2004 Reinhold Kainhofer <reinhold@kainhofer.com>
+  Copyright (c) 2006 Christian Weilbach <christian@whiletaker.homeip.net>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 #ifndef API_METAWEBLOG_H
 #define API_METAWEBLOG_H
@@ -40,13 +40,16 @@
   @author Christian Weilbach \<christian\@whiletaker.homeip.net\>
 
   \par Maintainer: Christian Weilbach \<christian\@whiletaker.homeip.net\>
- */
+*/
 
 namespace KBlog {
 /**
   @brief
-  A class that can be used for access to MetaWeblog API blogs. Almost every blog server
-  supports MetaWeblog API . Compared to Blogger API 1.0 it is a superset of functions added to the its definition. MetaWeblog API is much more functional, but has some drawbacks, e.g. security when compared to Blogger API 2.0 which is based on Atom API and quite new.
+  A class that can be used for access to MetaWeblog API blogs. Almost every
+  blog server supports MetaWeblog API . Compared to Blogger API 1.0 it is a
+  superset of functions added to the its definition. MetaWeblog API is much
+  more functional, but has some drawbacks, e.g. security when compared to
+  Blogger API 2.0 which is based on Atom API and quite new.
 
   @code
   APIBlog* myblog = new APIMetaWeblog( "http://example.com/xmlrpc/gateway.php" );
@@ -59,94 +62,99 @@ namespace KBlog {
 
   @author Christian Weilbach \<christian\@whiletaker.homeip.net\>
   @author Reinhold Kainhofer \<reinhold\@kainhofer.com\>
- */
+*/
 class KBLOG_EXPORT APIMetaWeblog : public APIBlog
 {
   Q_OBJECT
   public:
     /**
-         Create an object for MetaWeblog API
+      Create an object for MetaWeblog API
 
-         @param server is the url for the xmlrpc gateway.
+      @param server is the url for the xmlrpc gateway.
     */
     explicit APIMetaWeblog( const KUrl &server, QObject *parent = 0,
                             const char *name = 0 );
     virtual ~APIMetaWeblog();
 
     /**
-        Returns the API of the inherited object.
+      Returns the API of the inherited object.
     */
     QString interfaceName() const { return "MetaWeblog API"; }
     void setUrl( const KUrl &server );
 
     /**
-        Get information about the user from the blog. Note: This is not supported on the server side.
-	@see void userInfoRetrieved( const QString &nickname, const QString &userid, const QString &email )
+      Get information about the user from the blog.
+      Note: This is not supported on the server side.
+
+      @see void userInfoRetrieved( const QString &, const QString &, const QString & )
     */
     virtual void userInfo();
 
     /**
-        List the blogs available for this authentication on the server. Note: This is not supported on the server side.
-	@see void blogInfoRetrieved( const QString &id, const QString &name )
+      List the blogs available for this authentication on the server.
+      Note: This is not supported on the server side.
+
+      @see void blogInfoRetrieved( const QString &id, const QString &name )
     */
     virtual void listBlogs();
 
     /**
-        List recent postings on the server..
-	@see     void listedPosting( KBlog::BlogPosting &posting )
-        @see     void fetchedPosting( KBlog::BlogPosting &posting )
-        @see     void listPostingsFinished()
+      List recent postings on the server..
+      @see     void listedPosting( KBlog::BlogPosting &posting )
+      @see     void fetchedPosting( KBlog::BlogPosting &posting )
+      @see     void listPostingsFinished()
     */
     virtual void listPostings();
 
     /**
-        List the categories of the blog.
-	@see  void categoryInfoRetrieved( const QString &name, const QString &description )
-        @see  void listCategoriesFinished()
+      List the categories of the blog.
+      @see  void categoryInfoRetrieved( const QString &, const QString & )
+      @see  void listCategoriesFinished()
     */
     virtual void listCategories();
 
     /**
-        Fetch the Posting with postingId.
-        @param postingId is the id of the posting on the server.
+      Fetch the Posting with postingId.
+      @param postingId is the id of the posting on the server.
 
-        @see  void fetchedPosting( KBlog::BlogPosting &posting )
+      @see  void fetchedPosting( KBlog::BlogPosting &posting )
     */
     virtual void fetchPosting( const QString &postingId );
 
     /**
-        Modify a posting on server.
+      Modify a posting on server.
 
-        @param posting is used to send the modified posting including the correct postingId from it to the server.
+      @param posting is used to send the modified posting including the
+      correct postingId from it to the server.
     */
     virtual void modifyPosting( KBlog::BlogPosting *posting );
 
     /**
-        Create a new posting on server.
+      Create a new posting on server.
 
-        @param posting is send to the server.
+      @param posting is send to the server.
     */
     virtual void createPosting( KBlog::BlogPosting *posting );
 
     /**
-        Create a new media object, e.g. picture, on server.
+      Create a new media object, e.g. picture, on server.
 
-        @param media is send to the server.
+      @param media is send to the server.
     */
     virtual void createMedia( KBlog::BlogMedia *media );
 
     /**
-        Remove a posting from the server.
+      Remove a posting from the server.
 
-        @param postingId is the id of the posting to remove.
+      @param postingId is the id of the posting to remove.
 
-        @see void removePosting( KBlog::BlogPosting *posting )
+      @see void removePosting( KBlog::BlogPosting *posting )
     */
     virtual void removePosting( const QString &postingId );
 
-private:
+  private:
     class APIMetaWeblogPrivate;
-    APIMetaWeblogPrivate* const d;
+    APIMetaWeblogPrivate *const d;
 };
 
 }
