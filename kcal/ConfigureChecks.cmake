5278b098317416927dc591304ed6741a0864260d
Frank Osterfeld <frank.osterfeld@kdemail.net>
2007-03-24 19:24:31 +0000

    disable the check, too
    svn path=/trunk/KDE/kdepimlibs/; revision=646156

--- kcal/ConfigureChecks.cmake
+++ kcal/ConfigureChecks.cmake
@@ -5,5 +5,4 @@ check_include_files(stdint.h HAVE_STDINT_H)
 check_include_files(stdlib.h HAVE_STDLIB_H)
 check_include_files(wctype.h HAVE_WCTYPE_H)
 check_include_files(getopt.h HAVE_GETOPT_H)
-check_symbol_exists(snprintf "stdio.h" HAVE_SNPRINTF)   # vsnprintf.c
 check_symbol_exists(iswspace "wctype.h" HAVE_ISWSPACE)  # icalparser.c
