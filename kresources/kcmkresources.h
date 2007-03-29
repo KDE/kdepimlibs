a3691d36eb3a5d97a0b448b5116ce64661dabf2f
David Faure <faure@kde.org>
2007-03-29 18:37:19 +0000

    ah
    svn path=/trunk/KDE/kdepimlibs/; revision=647872

--- kresources/kcmkresources.h
+++ kresources/kcmkresources.h
@@ -22,7 +22,7 @@
 #ifndef KRESOURCES_KCMKRESOURCES_H
 #define KRESOURCES_KCMKRESOURCES_H
 
-#include <kresource.h>
+#include <kresources.h>
 #include <kcmodule.h>
 namespace KRES { class ConfigPage; }
 
