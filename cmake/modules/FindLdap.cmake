6b45f35feec0c08c41b09d88a8012c9f18bf65f1
Szombathelyi György <gyurco@freemail.hu>
2007-05-17 19:20:17 +0000

    Allow build without liblber.
    CCBUG: 145501
    
    svn path=/trunk/KDE/kdepimlibs/; revision=665745

--- cmake/modules/FindLdap.cmake
+++ cmake/modules/FindLdap.cmake
@@ -3,7 +3,7 @@
 #
 #  LDAP_FOUND - system has libldap
 #  LDAP_INCLUDE_DIR - the ldap include directory
-#  LDAP_LIBRARIES - libldap library
+#  LDAP_LIBRARIES - libldap + liblber (if found) library
 #  LBER_LIBRARIES - liblber library
 
 if(LDAP_INCLUDE_DIR AND LDAP_LIBRARIES)
@@ -39,11 +39,14 @@ endif(APPLE)
 
 if(LDAP_INCLUDE_DIR AND LDAP_LIBRARIES)
    set(LDAP_FOUND TRUE)
+   if(LBER_LIBRARIES)
+     set(LDAP_LIBRARIES ${LDAP_LIBRARIES} ${LBER_LIBRARIES})
+   endif(LBER_LIBRARIES)
 endif(LDAP_INCLUDE_DIR AND LDAP_LIBRARIES)
 
 if(LDAP_FOUND)
    if(NOT Ldap_FIND_QUIETLY)
-      message(STATUS "Found ldap: ${LDAP_LIBRARIES} ${LBER_LIBRARIES}")
+      message(STATUS "Found ldap: ${LDAP_LIBRARIES}")
    endif(NOT Ldap_FIND_QUIETLY)
 endif(LDAP_FOUND)
 
