17cd3121a0ecb6fa79b7550af7d66d47a8d723d5
Mike Arthur <mike@mikemcquaid.com>
2007-07-19 00:48:05 +0000

    Add copyright
    svn path=/trunk/KDE/kdepimlibs/; revision=689740

--- kblog/metaweblog_p.cpp
+++ kblog/metaweblog_p.cpp
@@ -2,6 +2,7 @@
   This file is part of the kblog library.
 
   Copyright (c) 2007 Christian Weilbach <christian@whiletaker.homeip.net>
+  Copyright (c) 2007 Mike Arthur <mike@mikearthur.co.uk>
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Library General Public
