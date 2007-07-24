f6a71d46ab469d4a4e39adc7c6b7c7e0861bf6e0
Antonio Aloisio <antonio.aloisio@gmail.com>
2007-07-24 17:32:22 +0000

    SVN_SILENT
    svn path=/trunk/KDE/kdepimlibs/; revision=691945

--- kblog/blog.h
+++ kblog/blog.h
@@ -222,7 +222,7 @@ class KBLOG_EXPORT APIBlog : public QObject
 
       @see listBlogs()
     */
-    virtual void listedBlogs(const QString&,const QString&,const QString& );
+    virtual void listedBlogs( const QString&, const QString&, const QString& );
 
     /**
       This signal is emitted when a listPostings() job fetches a posting
