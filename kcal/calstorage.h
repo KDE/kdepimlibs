5da0435e96f220ee6f1e7e8c34ba082411b8ae21
Allen Winter <winter@kde.org>
2007-08-07 02:30:06 +0000

    dpointer, de-inline, full doxyfication, cleaning
    svn path=/trunk/KDE/kdepimlibs/; revision=697173

--- kcal/calstorage.h
+++ kcal/calstorage.h
@@ -1,23 +1,31 @@
 /*
-    This file is part of the kcal library.
+  This file is part of the kcal library.
 
-    Copyright (c) 2002,2003 Cornelius Schumacher <schumacher@kde.org>
+  Copyright (c) 2002,2003 Cornelius Schumacher <schumacher@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
+/**
+  @file
+  This file is part of the API for handling calendar data and
+  defines the CalStorage abstract base class.
+
+  @author Cornelius Schumacher \<schumacher@kde.org\>
+*/
+
 #ifndef KCAL_CALSTORAGE_H
 #define KCAL_CALSTORAGE_H
 
@@ -28,29 +36,60 @@ namespace KCal {
 class Calendar;
 
 /**
-  This class provides the interface to the storage of a calendar.
+  @brief
+  An abstract base class that provides a calendar storage interface.
+
+  This is the base class for calendar storage. It provides an interface for the
+  loading and saving of calendars.
 */
 class KCAL_EXPORT CalStorage
 {
   public:
-    explicit CalStorage( Calendar *calendar ) : d ( 0 )
-    {
-      mCalendar = calendar;
-    }
-    virtual ~CalStorage() {}
+    /**
+      Construcst a new storage object for a calendar.
+      @param calendar is a pointer to a valid Calendar object.
+    */
+    explicit CalStorage( Calendar *calendar );
 
-    Calendar *calendar() const { return mCalendar; }
+    /**
+      Destuctor.
+    */
+    virtual ~CalStorage();
 
+    /**
+      Returns a pointer to the calendar whose storage is being managed.
+    */
+    Calendar *calendar() const;
+
+    /**
+      Opens the calendar for storage.
+      @return true if the open was successful; false otherwise.
+    */
     virtual bool open() = 0;
+
+    /**
+      Loads the calendar into memory.
+      @return true if the load was successful; false otherwise.
+    */
     virtual bool load() = 0;
+
+    /**
+      Saves the calendar.
+      @return true if the save was successful; false otherwise.
+    */
     virtual bool save() = 0;
+
+    /**
+      Closes the calendar storage.
+      @return true if the close was successful; false otherwise.
+    */
     virtual bool close() = 0;
 
   private:
-    Calendar *mCalendar;
-
+    //@cond PRIVATE
     class Private;
     Private *const d;
+    //@endcond
 };
 
 }
