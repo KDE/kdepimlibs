a559ed5f3675a259014862ba0aecbbb0c466cb99
Allen Winter <winter@kde.org>
2007-08-30 14:14:21 +0000

    KUIT strings and some minor code style fixing
    svn path=/trunk/KDE/kdepimlibs/; revision=706472

--- kcal/resourcecachedconfig.cpp
+++ kcal/resourcecachedconfig.cpp
@@ -1,22 +1,22 @@
 /*
-    This file is part of the kcal library.
+  This file is part of the kcal library.
 
-    Copyright (c) 2004 Cornelius Schumacher <schumacher@kde.org>
+  Copyright (c) 2004 Cornelius Schumacher <schumacher@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
 #include "resourcecachedconfig.moc"
@@ -51,22 +51,31 @@ class ResourceCachedConfigPrivate
 };
 
 class KCal::ResourceCachedReloadConfig::Private
-  : public ResourceCachedConfigPrivate {};
+  : public ResourceCachedConfigPrivate
+{
+};
 
 class KCal::ResourceCachedSaveConfig::Private
-  : public ResourceCachedConfigPrivate {};
+  : public ResourceCachedConfigPrivate
+{
+};
 
 ResourceCachedReloadConfig::ResourceCachedReloadConfig( QWidget *parent )
   : QWidget( parent ), d( new KCal::ResourceCachedReloadConfig::Private() )
 {
   QBoxLayout *topLayout = new QVBoxLayout( this );
 
-  QGroupBox* groupBox = new QGroupBox( i18n("Automatic Reload"), this );
+  QGroupBox *groupBox = new QGroupBox( i18nc( "@title:group", "Automatic Reload" ), this );
   topLayout->addWidget( groupBox );
-  QRadioButton* noAutomaticReload = new QRadioButton( i18n("Never"), groupBox );
-  QRadioButton* automaticReloadOnStartup = new QRadioButton( i18n("On startup"), groupBox );
-  QRadioButton* intervalRadio = new QRadioButton( i18n("Regular interval"),
-                                     groupBox );
+  QRadioButton *noAutomaticReload =
+    new QRadioButton(
+      i18nc( "@option:radio never reload the cache", "Never" ), groupBox );
+  QRadioButton *automaticReloadOnStartup =
+    new QRadioButton(
+      i18nc( "@option:radio reload the cache on startup", "On startup" ), groupBox );
+  QRadioButton *intervalRadio =
+    new QRadioButton(
+      i18nc( "@option:radio reload the cache at regular intervals", "Regular interval" ), groupBox );
   d->mGroup = new QButtonGroup( this );
   d->mGroup->addButton( noAutomaticReload, 0 );
   d->mGroup->addButton( automaticReloadOnStartup, 1 );
@@ -74,8 +83,9 @@ ResourceCachedReloadConfig::ResourceCachedReloadConfig( QWidget *parent )
 
   connect( intervalRadio, SIGNAL( toggled( bool ) ),
            SLOT( slotIntervalToggled( bool ) ) );
+
   KHBox *intervalBox = new KHBox;
-  new QLabel( i18n("Interval in minutes"), intervalBox );
+  new QLabel( i18nc( "@label:spinbox", "Interval in minutes" ), intervalBox );
   d->mIntervalSpin = new QSpinBox( intervalBox );
   d->mIntervalSpin->setRange( 1, 900 );
   d->mIntervalSpin->setEnabled( false );
@@ -87,8 +97,6 @@ ResourceCachedReloadConfig::ResourceCachedReloadConfig( QWidget *parent )
   vbox->addWidget(intervalBox);
   vbox->addStretch(1);
   groupBox->setLayout(vbox);
-
-
 }
 
 void ResourceCachedReloadConfig::loadSettings( ResourceCached *resource )
@@ -105,41 +113,48 @@ void ResourceCachedReloadConfig::saveSettings( ResourceCached *resource )
 
 void ResourceCachedReloadConfig::slotIntervalToggled( bool checked )
 {
-  if ( checked ) d->mIntervalSpin->setEnabled( true );
-  else d->mIntervalSpin->setEnabled( false );
+  if ( checked ) {
+    d->mIntervalSpin->setEnabled( true );
+  } else {
+    d->mIntervalSpin->setEnabled( false );
+  }
 }
-
 
 ResourceCachedSaveConfig::ResourceCachedSaveConfig( QWidget *parent )
   : QWidget( parent ), d( new KCal::ResourceCachedSaveConfig::Private() )
 {
   QBoxLayout *topLayout = new QVBoxLayout( this );
 
-  QGroupBox* groupBox = new QGroupBox( i18n("Automatic Save"), this );
+  QGroupBox *groupBox = new QGroupBox( i18nc( "@title:group", "Automatic Save" ), this );
   d->mGroup = new QButtonGroup( this );
   topLayout->addWidget( groupBox );
-  QRadioButton* never = new QRadioButton( i18n("Never"), groupBox );
-  QRadioButton* onExit = new QRadioButton( i18n("On exit"), groupBox );
+  QRadioButton *never =
+    new QRadioButton(
+      i18nc( "@option:radio never save the cache automatically", "Never" ), groupBox );
+  QRadioButton *onExit =
+    new QRadioButton(
+      i18nc( "@option:radio save the cache on exit", "On exit" ), groupBox );
 
-  QRadioButton *intervalRadio = new QRadioButton( i18n("Regular interval"),
-                                                  groupBox );
+  QRadioButton *intervalRadio =
+    new QRadioButton(
+      i18nc( "@option:radio save the cache at regular intervals", "Regular interval" ), groupBox );
 
   d->mGroup = new QButtonGroup( this );
   d->mGroup->addButton( never, 0 );
   d->mGroup->addButton( onExit, 1 );
   d->mGroup->addButton( intervalRadio, 2 );
 
-
   connect( intervalRadio, SIGNAL( toggled( bool ) ),
            SLOT( slotIntervalToggled( bool ) ) );
+
   KHBox *intervalBox = new KHBox;
-  new QLabel( i18n("Interval in minutes"), intervalBox );
+  new QLabel( i18nc( "@label:spinbox", "Interval in minutes" ), intervalBox );
   d->mIntervalSpin = new QSpinBox( intervalBox );
   d->mIntervalSpin->setRange( 1, 900 );
   d->mIntervalSpin->setEnabled( false );
 
-  QRadioButton* delay = new QRadioButton( i18n("Delayed after changes"), groupBox );
-  QRadioButton* every = new QRadioButton( i18n("On every change"), groupBox );
+  QRadioButton *delay = new QRadioButton( i18nc( "@option:radio save the cache after some delay", "Delayed after changes" ), groupBox );
+  QRadioButton *every = new QRadioButton( i18nc( "@option:radio save the cache after every modification", "On every change" ), groupBox );
 
   QVBoxLayout *vbox = new QVBoxLayout;
   vbox->addWidget(never);
@@ -167,6 +182,9 @@ void ResourceCachedSaveConfig::saveSettings( ResourceCached *resource )
 
 void ResourceCachedSaveConfig::slotIntervalToggled( bool checked )
 {
-  if ( checked ) d->mIntervalSpin->setEnabled( true );
-  else d->mIntervalSpin->setEnabled( false );
+  if ( checked ) {
+    d->mIntervalSpin->setEnabled( true );
+  } else {
+    d->mIntervalSpin->setEnabled( false );
+  }
 }
