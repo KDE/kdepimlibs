aadc11ff6fd26fb5fe8adce9ac0d9cda725cff21
Allen Winter <winter@kde.org>
2007-09-09 18:05:36 +0000

    doxygen needs '/**' to start a comment block it will recognize. SVN_SILENT:
    svn path=/trunk/KDE/kdepimlibs/; revision=710347

--- kcal/calendarnull.h
+++ kcal/calendarnull.h
@@ -18,7 +18,7 @@
   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
   Boston, MA 02110-1301, USA.
 */
-/*
+/**
   @file
   This file is part of the API for handling calendar data and
   defines the CalendarNull class.
