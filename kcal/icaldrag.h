563b0df5f5b46d0cb438e65c7eeef7dcf55fc565
Allen Winter <winter@kde.org>
2007-09-11 14:19:57 +0000

    update the CHECKLIST SVN_SILENT:
    svn path=/trunk/KDE/kdepimlibs/; revision=711131

--- kcal/icaldrag.h
+++ kcal/icaldrag.h
@@ -1,23 +1,23 @@
 /*
-    This file is part of the kcal library.
+  This file is part of the kcal library.
 
-    Copyright (c) 1998 Preston Brown <pbrown@kde.org>
-    Copyright (c) 2001-2003 Cornelius Schumacher <schumacher@kde.org>
+  Copyright (c) 1998 Preston Brown <pbrown@kde.org>
+  Copyright (c) 2001-2003 Cornelius Schumacher <schumacher@kde.org>
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Library General Public
-    License as published by the Free Software Foundation; either
-    version 2 of the License, or (at your option) any later version.
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Library General Public
+  License as published by the Free Software Foundation; either
+  version 2 of the License, or (at your option) any later version.
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Library General Public License for more details.
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Library General Public License for more details.
 
-    You should have received a copy of the GNU Library General Public License
-    along with this library; see the file COPYING.LIB.  If not, write to
-    the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
-    Boston, MA 02110-1301, USA.
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to
+  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+  Boston, MA 02110-1301, USA.
 */
 
 #ifndef KCAL_ICALDRAG_H
@@ -37,22 +37,25 @@ class Calendar;
 */
 namespace ICalDrag
 {
-    /**
-      Mime-type of iCalendar
-    */
-    KCAL_EXPORT QString mimeType();
-    /**
-      Sets the iCalendar representation as data of the drag object
-    */
-    KCAL_EXPORT bool populateMimeData( QMimeData *e, Calendar *cal );
-    /**
-      Return, if drag&drop object can be decode to iCalendar.
-    */
-    KCAL_EXPORT bool canDecode( const QMimeData * );
-    /**
-      Decode drag&drop object to iCalendar component \a cal.
-    */
-    KCAL_EXPORT bool fromMimeData( const QMimeData *e, Calendar *cal );
+  /**
+    Mime-type of iCalendar
+  */
+  KCAL_EXPORT QString mimeType();
+
+  /**
+    Sets the iCalendar representation as data of the drag object
+  */
+  KCAL_EXPORT bool populateMimeData( QMimeData *e, Calendar *cal );
+
+  /**
+    Return, if drag&drop object can be decode to iCalendar.
+  */
+  KCAL_EXPORT bool canDecode( const QMimeData * );
+
+  /**
+    Decode drag&drop object to iCalendar component \a cal.
+  */
+  KCAL_EXPORT bool fromMimeData( const QMimeData *e, Calendar *cal );
 }
 
 }
