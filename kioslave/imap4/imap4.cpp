b35c00016dd671aca964907d76aa5a2ed4bc15cc
Jakub Stachowski <qbast@go2.pl>
2008-05-04 10:32:25 +0000

    Default port for imap and imaps. Before after typing imap://server/ in konqueror, slave would try to connect to port 0.
    svn path=/trunk/KDE/kdepimlibs/; revision=803849

--- kioslave/imap4/imap4.cpp
+++ kioslave/imap4/imap4.cpp
@@ -95,6 +95,8 @@ extern "C" {
 
 #define IMAP_PROTOCOL "imap"
 #define IMAP_SSL_PROTOCOL "imaps"
+#define IMAP_PORT 143
+#define IMAPS_PORT 993
 
 using namespace KIO;
 
@@ -631,7 +633,10 @@ IMAP4Protocol::setHost (const QString & _host, quint16 _port,
     if (!myHost.isEmpty ())
       closeConnection ();
     myHost = _host;
-    myPort = _port;
+    if (_port == 0) 
+        myPort = (mySSL) ? IMAPS_PORT : IMAP_PORT;
+    else 
+        myPort = _port;
     myUser = _user;
     myPass = _pass;
   }
