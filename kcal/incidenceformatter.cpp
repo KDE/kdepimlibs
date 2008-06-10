f50b27bf1a1c4f7e67cb8aaaebd79b2cb2ed2c96
Volker Krause <vkrause@kde.org>
2008-06-10 17:12:17 +0000

    Merged revisions 791200 via svnmerge from https://vkrause@svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r791200 | vkrause | 2008-03-28 19:02:57 +0100 (Fri, 28 Mar 2008) | 2 lines
      
      Improve display of iTIP counter proposals.
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepimlibs/; revision=819132

--- kcal/incidenceformatter.cpp
+++ kcal/incidenceformatter.cpp
@@ -1394,6 +1394,13 @@ QString IncidenceFormatter::formatICalInvitation( QString invitation, Calendar *
     break;
 
   case iTIPCounter:
+    html += helper->makeLink( "accept_counter", i18n("[Accept]") );
+    html += "&nbsp;";
+    html += helper->makeLink( "decline_counter", i18n("[Decline]") );
+    html += "&nbsp;";
+    html += helper->makeLink( "check_calendar", i18n("[Check my calendar]" ) );
+    break;
+
   case iTIPDeclineCounter:
   case iTIPNoMethod:
     break;
