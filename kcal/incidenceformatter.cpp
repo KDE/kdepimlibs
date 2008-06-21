8bd96fe7f9b344587f8fa4de09e1a268ea97a209
Thomas McGuire <mcguire@kde.org>
2008-06-21 16:07:33 +0000

    Merged revisions 813668,817244,817246 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise4/kdepimlibs
    ........
      r813668 | vkrause | 2008-05-28 14:55:25 +0200 (Wed, 28 May 2008) | 7 lines
      
      Merge SVN commit 800441 by ervin from enterprise3 branch:
      
      Enable the old compat mode also if the version string ends
      with "-kolab-nocaps".
      
      Should fixes kolab/issue2443.
    ........
      r817244 | vkrause | 2008-06-05 17:56:12 +0200 (Thu, 05 Jun 2008) | 7 lines
      
      Merge SVN commit 780286 by vkrause from enterprise branch:
      
      Try to ignore incidences in shared calendars, they are most likely
      events created by other users who got the same invitation.
      
      Kolab issue 2312
    ........
      r817246 | vkrause | 2008-06-05 18:00:27 +0200 (Thu, 05 Jun 2008) | 7 lines
      
      Merge SVN commit 780298 by vkrause from enterprise branch:
      
      Always show the action links when we cannot lookup the current incidence
      in the calendar.
      
      Kolab issue 2312
    ........
    
    svn path=/trunk/KDE/kdepimlibs/; revision=822823

--- kcal/incidenceformatter.cpp
+++ kcal/incidenceformatter.cpp
@@ -1247,6 +1247,22 @@ Calendar *InvitationFormatterHelper::calendar() const
   return 0;
 }
 
+// Check if the given incidence is likely one that we own instead one from
+// a shared calendar (Kolab-specific)
+static bool incidenceOwnedByMe( Calendar* calendar, Incidence *incidence )
+{
+  CalendarResources* cal = dynamic_cast<CalendarResources*>( calendar );
+  if ( !cal || !incidence )
+    return true;
+  ResourceCalendar* res = cal->resource( incidence );
+  if ( !res )
+    return true;
+  const QString subRes = res->subresourceIdentifier( incidence );
+  if ( !subRes.contains( "/.INBOX.directory/" ) )
+    return false;
+  return true;
+}
+
 QString IncidenceFormatter::formatICalInvitation( QString invitation, Calendar *mCalendar,
     InvitationFormatterHelper *helper )
 {
@@ -1271,10 +1287,12 @@ QString IncidenceFormatter::formatICalInvitation( QString invitation, Calendar *
   Incidence *existingIncidence = 0;
   if ( helper->calendar() ) {
     existingIncidence = helper->calendar()->incidence( incBase->uid() );
+    if ( !incidenceOwnedByMe( helper->calendar(), existingIncidence ) )
+      existingIncidence = 0;
     if ( !existingIncidence ) {
       const Incidence::List list = helper->calendar()->incidences();
       for ( Incidence::List::ConstIterator it = list.begin(), end = list.end(); it != end; ++it ) {
-        if ( (*it)->schedulingID() == incBase->uid() ) {
+        if ( (*it)->schedulingID() == incBase->uid() && incidenceOwnedByMe( helper->calendar(), *it ) ) {
           existingIncidence = *it;
           break;
         }
@@ -1345,7 +1363,7 @@ QString IncidenceFormatter::formatICalInvitation( QString invitation, Calendar *
     }
     html += "<td>";
 
-    if ( helper->calendar() && !existingIncidence ) {
+    if ( !existingIncidence ) {
       // Accept
       html += helper->makeLink( "accept", i18nc( "accept to-do request", "[Accept]" ) );
       html += "</td><td> &nbsp; </td><td>";
