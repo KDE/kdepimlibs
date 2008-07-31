e5400c3446b4f1997987b182577a981e4a2c29f8
Allen Winter <winter@kde.org>
2008-07-31 00:01:21 +0000

    fix to find libical and libicalss stuff
    svn path=/trunk/KDE/kdepimlibs/; revision=839868

--- cmake/modules/FindLibical.cmake
+++ cmake/modules/FindLibical.cmake
@@ -1,7 +1,7 @@
 # Find Libical
 #
 #  LIBICAL_FOUND - system has Libical
-#  LIBICAL_INCLUDE_DIRS - the Libical include directory
+#  LIBICAL_INCLUDE_DIRS - the Libical include directories
 #  LIBICAL_LIBRARIES - The libraries needed to use Libical
 
 # Copyright (c) 2008, Allen Winter <winter@kde.org>
@@ -9,21 +9,23 @@
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-SET (LIBICAL_FIND_REQUIRED ${Libical_FIND_REQUIRED})
-if (LIBICAL_INCLUDE_DIR AND LIBICAL_LIBRARIES)
+set(LIBICAL_FIND_REQUIRED ${Libical_FIND_REQUIRED})
+if(LIBICAL_INCLUDE_DIRS AND LIBICAL_LIBRARIES)
 
   # Already in cache, be silent
   set(LIBICAL_FIND_QUIETLY TRUE)
 
-else (LIBICAL_INCLUDE_DIR AND LIBICAL_LIBRARIES)
+endif(LIBICAL_INCLUDE_DIRS AND LIBICAL_LIBRARIES)
 
-  find_path(LIBICAL_INCLUDE_DIR ical.h)
-  find_library(LIBICAL_LIBRARIES NAMES ical )
+find_path(LIBICAL_INCLUDE_DIR NAMES ical.h PATH_SUFFIXES libical)
+find_path(LIBICALSS_INCLUDE_DIR NAMES icalss.h PATH_SUFFIXES libicalss)
+set(LIBICAL_INCLUDE_DIRS ${LIBICAL_INCLUDE_DIR} ${LIBICALSS_INCLUDE_DIR})
 
-  include(FindPackageHandleStandardArgs)
-  FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBICAL DEFAULT_MSG
-                                    LIBICAL_LIBRARIES LIBICAL_INCLUDE_DIR)
+find_library(LIBICAL_LIBRARY NAMES ical)
+find_library(LIBICALSS_LIBRARY NAMES icalss)
+set(LIBICAL_LIBRARIES ${LIBICAL_LIBRARY} ${LIBICALSS_LIBRARY})
 
-  mark_as_advanced(LIBICAL_INCLUDE_DIR LIBICAL_LIBRARIES)
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBICAL DEFAULT_MSG LIBICAL_LIBRARIES LIBICAL_INCLUDE_DIRS)
 
-endif (LIBICAL_INCLUDE_DIR AND LIBICAL_LIBRARIES)
+mark_as_advanced(LIBICAL_INCLUDE_DIRS LIBICAL_LIBRARIES)
