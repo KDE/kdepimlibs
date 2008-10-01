c3faace4f0062883b503c6dfd5de2494feaf630c
Laurent Montel <montel@kde.org>
2008-10-01 06:58:53 +0000

    Not send a error message until 13 october otherwise we can't compile kdepimlins for the moment And for the moment libical>=0.33 is not required
    svn path=/trunk/KDE/kdepimlibs/; revision=866508

--- cmake/modules/FindLibical.cmake
+++ cmake/modules/FindLibical.cmake
@@ -69,7 +69,10 @@ if(LIBICAL_INCLUDE_DIRS AND LIBICAL_LIBRARIES)
       set(LIBICAL_LIBRARIES "")
     endif(NOT LIBICAL_VERSION_OK)
   else(COMPILE_RESULT AND RUN_RESULT EQ 1)
-    message(FATAL_ERROR "Unable to compile or run the libical version detection program.")
+    #Enable this line the 13 october
+    #message(FATAL_ERROR "Unable to compile or run the libical version detection program.")
+    #Delete this line the 13 october
+    message(STATUS "Unable to compile or run the libical version detection program.")
   endif(COMPILE_RESULT AND RUN_RESULT EQUAL 1)
 
 endif(LIBICAL_INCLUDE_DIRS AND LIBICAL_LIBRARIES)
