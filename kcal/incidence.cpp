5f081eae7bb8865a2eee721406582e8f5ed45636
Thomas McGuire <mcguire@kde.org>
2008-10-21 18:58:34 +0000

    Backport r874506 by tmcguire from trunk to the 4.1 branch:
    Merged revisions 874469 via svnmerge from 
    svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise4/kdepimlibs
    
    ........
      r874469 | tilladam | 2008-10-21 19:08:48 +0200 (Tue, 21 Oct 2008) | 1 line
      
      Do not honor read only state of incidences in setRelatedTo and setRelatedToUid, otherwise the internal parent/child structures are not built properly, and thus not properly cleaned up, which leads to crashes. Whatever this was supposed to prevent, it will have to be caught at a higher level.
    ........
    
    
    svn path=/branches/KDE/4.1/kdepimlibs/; revision=874510

--- kcal/incidence.cpp
+++ kcal/incidence.cpp
@@ -464,7 +464,7 @@ QString Incidence::categoriesStr() const
 
 void Incidence::setRelatedToUid( const QString &relatedToUid )
 {
-  if ( mReadOnly || d->mRelatedToUid == relatedToUid ) {
+  if ( d->mRelatedToUid == relatedToUid ) {
     return;
   }
   d->mRelatedToUid = relatedToUid;
@@ -478,7 +478,7 @@ QString Incidence::relatedToUid() const
 
 void Incidence::setRelatedTo( Incidence *incidence )
 {
-  if ( mReadOnly || d->mRelatedTo == incidence ) {
+  if ( d->mRelatedTo == incidence ) {
     return;
   }
   if ( d->mRelatedTo ) {
