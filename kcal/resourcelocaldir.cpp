57163aa0197f0644394d30ef72f4a62b10577f2f
Sergio Luis Martins <iamsergio@gmail.com>
2009-04-16 20:25:38 +0000

    Change the way resource local dir handles changes, this fixes some segfaults and avoids closing the calendar each time an incidence is modified.
    Review: http://reviewboard.kde.org/r/516/
    BUG: 180221
    BUG: 187595
    
    
    svn path=/trunk/KDE/kdepimlibs/; revision=955052

--- kcal/resourcelocaldir.cpp
+++ kcal/resourcelocaldir.cpp
@@ -2,6 +2,7 @@
   This file is part of the kcal library.
 
   Copyright (c) 2003 Cornelius Schumacher <schumacher@kde.org>
+  Copyright (c) 2009 Sergio Martins <iamsergio@gmail.com>
 
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Library General Public
@@ -29,6 +30,8 @@
 
 #include "kresources/configwidget.h"
 
+#include <kcal/assignmentvisitor.h>
+#include <kcal/comparisonvisitor.h>
 #include <kdebug.h>
 #include <klocale.h>
 #include <kconfig.h>
@@ -43,26 +46,27 @@
 #include <stdlib.h>
 
 #include "resourcelocaldir.moc"
+#include "resourcelocaldir_p.moc"
 
 using namespace KCal;
 
 ResourceLocalDir::ResourceLocalDir()
-  : ResourceCached(), d( new KCal::ResourceLocalDir::Private )
+  : ResourceCached(), d( new KCal::ResourceLocalDir::Private( this ) )
 {
-  d->init( this );
+  d->init();
 }
 
 ResourceLocalDir::ResourceLocalDir( const KConfigGroup &group )
-  : ResourceCached( group ), d( new KCal::ResourceLocalDir::Private )
+  : ResourceCached( group ), d( new KCal::ResourceLocalDir::Private( this ) )
 {
   readConfig( group );
-  d->init( this );
+  d->init();
 }
 
 ResourceLocalDir::ResourceLocalDir( const QString &dirName )
-  : ResourceCached(), d( new KCal::ResourceLocalDir::Private( dirName ) )
+  : ResourceCached(), d( new KCal::ResourceLocalDir::Private( dirName, this ) )
 {
-  d->init( this );
+  d->init();
 }
 
 void ResourceLocalDir::readConfig( const KConfigGroup &group )
@@ -80,18 +84,21 @@ void ResourceLocalDir::writeConfig( KConfigGroup &group )
   group.writePathEntry( "CalendarURL", d->mURL.prettyUrl() );
 }
 
-void ResourceLocalDir::Private::init( ResourceLocalDir *rdir )
+void ResourceLocalDir::Private::init( )
 {
-  rdir->setType( "dir" );
+  mResource->setType( "dir" );
 
-  rdir->setSavePolicy( SaveDelayed );
+  mResource->setSavePolicy( SaveDelayed );
 
-  rdir->connect( &mDirWatch, SIGNAL( dirty( const QString & ) ),
-                 SLOT( reload( const QString & ) ) );
-  rdir->connect( &mDirWatch, SIGNAL( created( const QString & ) ),
-                 SLOT( reload( const QString & ) ) );
-  rdir->connect( &mDirWatch, SIGNAL( deleted( const QString & ) ),
-                 SLOT( reload( const QString & ) ) );
+  connect( &mDirWatch, SIGNAL( dirty( const QString & ) ),
+           this, SLOT( updateIncidenceInCalendar( const QString & ) ) );
+  connect( &mDirWatch, SIGNAL( created( const QString & ) ),
+           this, SLOT( addIncidenceToCalendar( const QString & ) ) );
+  connect( &mDirWatch, SIGNAL( deleted( const QString & ) ),
+           this, SLOT( deleteIncidenceFromCalendar( const QString & ) ) );
+
+  connect ( this, SIGNAL(resourceChanged( ResourceCalendar *)),
+            mResource, SIGNAL(resourceChanged( ResourceCalendar *)) );
 
   mLock = new KABC::Lock( mURL.path() );
 
@@ -142,13 +149,13 @@ bool ResourceLocalDir::doLoad( bool )
   const QStringList entries = dir.entryList( QDir::Files | QDir::Readable );
 
   bool success = true;
-  for ( int i = 0, count = entries.count(); i < count; ++i ) {
-    if ( entries[i].contains( QRegExp( "(~|\\.new|\\.tmp)$" ) ) ||
-         entries[i].startsWith( d->mURL.path() + "/qt_temp." ) ) {
+
+  foreach ( const QString &entry, entries ) {
+    if ( d->isTempFile( entry ) ) {
       continue;  // backup or temporary file, ignore it
     }
 
-    const QString fileName = dirName + '/' + entries[i];
+    const QString fileName = dirName + '/' + entry;
     kDebug() << " read '" << fileName << "'";
     CalendarLocal cal( calendar()->timeSpec() );
     if ( !doFileLoad( cal, fileName ) ) {
@@ -161,18 +168,7 @@ bool ResourceLocalDir::doLoad( bool )
 
 bool ResourceLocalDir::doFileLoad( CalendarLocal &cal, const QString &fileName )
 {
-  if ( !cal.load( fileName ) ) {
-    return false;
-  }
-  Incidence::List incidences = cal.rawIncidences();
-  Incidence::List::ConstIterator it;
-  for ( it = incidences.constBegin(); it != incidences.constEnd(); ++it ) {
-    Incidence *i = *it;
-    if ( i ) {
-      calendar()->addIncidence( i->clone() );
-    }
-  }
-  return true;
+  return d->doFileLoad( cal, fileName, false );
 }
 
 bool ResourceLocalDir::doSave( bool syncCache )
@@ -218,23 +214,8 @@ KABC::Lock *ResourceLocalDir::lock()
 {
   return d->mLock;
 }
-
-void ResourceLocalDir::reload( const QString &file )
-{
-  kDebug();
-
-  if ( !isOpen() ||
-       file.contains( QRegExp( "(~|\\.new|\\.tmp)$" ) ) ||
-       file.startsWith( d->mURL.path() + "/qt_temp." ) ) {
-    return;
-  }
-
-  kDebug() << "  File: '" << file << "'";
-
-  calendar()->close();
-  load();
-
-  emit resourceChanged( this );
+void ResourceLocalDir::reload( const QString &file ) {
+  Q_UNUSED( file );
 }
 
 bool ResourceLocalDir::deleteEvent( Event *event )
@@ -312,4 +293,104 @@ bool ResourceLocalDir::Private::deleteIncidenceFile( Incidence *incidence )
   bool removed = file.remove();
   mDirWatch.startScan();
   return removed;
+}
+
+bool ResourceLocalDir::Private::isTempFile( const QString &fileName ) const
+{
+  return fileName.contains( QRegExp( "(~|\\.new|\\.tmp)$" ) )       ||
+         QFileInfo( fileName ).fileName().startsWith( "qt_temp." )  ||
+         fileName == mURL.path();
+}
+
+void ResourceLocalDir::Private::addIncidenceToCalendar( const QString &file )
+{
+
+  if ( mResource->isOpen() &&
+       !isTempFile( file ) &&
+       !mResource->calendar()->incidence( getUidFromFileName( file ) ) ) {
+
+    CalendarLocal cal( mResource->calendar()->timeSpec() );
+    if ( doFileLoad( cal, file, true ) ) {
+      emit resourceChanged( mResource );
+    }
+  }
+}
+
+void ResourceLocalDir::Private::updateIncidenceInCalendar( const QString &file )
+{
+  if ( mResource->isOpen() && !isTempFile( file ) ) {
+    CalendarLocal cal( mResource->calendar()->timeSpec() );
+    if ( doFileLoad( cal, file, true ) ) {
+      emit resourceChanged( mResource );
+    }
+  }
+}
+
+QString ResourceLocalDir::Private::getUidFromFileName( const QString &fileName )
+{
+  return QFileInfo( fileName ).fileName();
+}
+
+void ResourceLocalDir::Private::deleteIncidenceFromCalendar( const QString &file )
+{
+
+  if ( mResource->isOpen() && !isTempFile( file ) ) {
+    Incidence *inc = mResource->calendar()->incidence( getUidFromFileName( file ) );
+
+    if ( inc ) {
+      mResource->calendar()->deleteIncidence( inc );
+      emit resourceChanged( mResource );
+    }
+  }
+}
+
+bool ResourceLocalDir::Private::doFileLoad( CalendarLocal &cal,
+                                            const QString &fileName,
+                                            const bool replace )
+{
+  if ( !cal.load( fileName ) ) {
+    return false;
+  }
+  Incidence::List incidences = cal.rawIncidences();
+  Incidence::List::ConstIterator it;
+  Incidence *inc;
+  ComparisonVisitor compVisitor;
+  AssignmentVisitor assVisitor;
+  for ( it = incidences.constBegin(); it != incidences.constEnd(); ++it ) {
+    Incidence *i = *it;
+    if ( i ) {
+      // should we replace, and does the incidence exist in calendar?
+      if ( replace && ( inc = mResource->calendar()->incidence( i->uid() ) ) ) {
+        if ( compVisitor.compare( i, inc ) ) {
+          // no need to do anything
+          return false;
+        } else {
+          inc->startUpdates();
+
+          bool assignResult = assVisitor.assign( inc, i );
+
+          if ( assignResult ) {
+            if ( !inc->relatedToUid().isEmpty() ) {
+              QString uid = inc->relatedToUid();
+              inc->setRelatedTo( mResource->calendar()->incidence( uid ) );
+            }
+            inc->updated();
+            inc->endUpdates();
+          } else {
+            inc->endUpdates();
+            kWarning( 5800 ) << "Incidence (uid=" << inc->uid()
+                             << ", summary=" << inc->summary()
+                             << ") changed type. Replacing it.";
+
+            mResource->calendar()->deleteIncidence( inc );
+            delete inc;
+            mResource->calendar()->addIncidence( i->clone() );
+          }
+        }
+      } else {
+        mResource->calendar()->addIncidence( i->clone() );
+      }
+    }
+  }
+  return true;
 }
