3f8bdbf87c6bff1eab8b05cdb847e9288339bcc7
Sergio Luis Martins <iamsergio@gmail.com>
2009-05-12 21:02:06 +0000

    Fix kcal's html export. Replace i18nc("@info", with i18nc("@info/plain" so the resulting html file doesn't get full of <html> tags.
    Shouldn't give too much work to translators as the actual strings remain untouched.
    
    BUG: 192316
    
    svn path=/trunk/KDE/kdepimlibs/; revision=967201

--- kcal/htmlexport.cpp
+++ kcal/htmlexport.cpp
@@ -504,7 +504,7 @@ void HtmlExport::createTodo( QTextStream *ts, Todo *todo )
     *ts << " class=\"done\"";
   }
   *ts << ">\n";
-  *ts << "    " << i18nc( "@info to-do percent complete",
+  *ts << "    " << i18nc( "@info/plain to-do percent complete",
                           "%1 %", todo->percentComplete() ) << "\n";
   *ts << "  </td>\n";
 
@@ -667,7 +667,7 @@ QString HtmlExport::breakString( const QString &text )
 void HtmlExport::createFooter( QTextStream *ts )
 {
   // FIXME: Implement this in a translatable way!
-  QString trailer = i18nc( "@info", "This page was created " );
+  QString trailer = i18nc( "@info/plain", "This page was created " );
 
 /*  bool hasPerson = false;
   bool hasCredit = false;
@@ -675,27 +675,27 @@ void HtmlExport::createFooter( QTextStream *ts )
   QString mail, name, credit, creditURL;*/
   if ( !d->mSettings->eMail().isEmpty() ) {
     if ( !d->mSettings->name().isEmpty() ) {
-      trailer += i18nc( "@info page creator email link with name",
+      trailer += i18nc( "@info/plain page creator email link with name",
                         "by <link url='mailto:%1'>%2</link>",
                         d->mSettings->eMail(), d->mSettings->name() );
     } else {
-      trailer += i18nc( "@info page creator email link",
+      trailer += i18nc( "@info/plain page creator email link",
                         "by <link url='mailto:%1'>%2</link>",
                         d->mSettings->eMail(), d->mSettings->eMail() );
     }
   } else {
     if ( !d->mSettings->name().isEmpty() ) {
-      trailer += i18nc( "@info page creator name only",
+      trailer += i18nc( "@info/plain page creator name only",
                         "by %1 ", d->mSettings->name() );
     }
   }
   if ( !d->mSettings->creditName().isEmpty() ) {
     if ( !d->mSettings->creditURL().isEmpty() ) {
-      trailer += i18nc( "@info page credit with name and link",
+      trailer += i18nc( "@info/plain page credit with name and link",
                         "with <link url='%1'>%2</link>",
                         d->mSettings->creditURL(), d->mSettings->creditName() );
     } else {
-      trailer += i18nc( "@info page credit name only",
+      trailer += i18nc( "@info/plain page credit name only",
                         "with %1", d->mSettings->creditName() );
     }
   }
@@ -762,7 +762,7 @@ void HtmlExport::addHoliday( const QDate &date, const QString &name )
   if ( d->mHolidayMap[date].isEmpty() ) {
     d->mHolidayMap[date] = name;
   } else {
-    d->mHolidayMap[date] = i18nc( "@info holiday by date and name",
+    d->mHolidayMap[date] = i18nc( "@info/plain holiday by date and name",
                                   "%1, %2", d->mHolidayMap[date], name );
   }
 }
