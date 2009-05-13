88fd331c0b8919e3e7fe01bb37e98c819582aade
Thomas McGuire <mcguire@kde.org>
2009-05-13 13:54:44 +0000

    Merged revisions 966698 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/KDE/4.2/kdepimlibs
    ........
      r966698 | smartins | 2009-05-11 21:04:27 +0200 (Mon, 11 May 2009) | 7 lines
      
      Backport r966325 by smartins from trunk to the 4.2 branch:
      
      Don't do setRelatedTo() when dissociating recurring to-dos, otherwise the new to-do will appear as a child.
      
      From what I was told, it was originaly planned to set a relation with reltype SIBLING when dissociating to-dos, but currently kcal
      only supports reltype PARENT.
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepimlibs/; revision=967505

--- kcal/calendar.cpp
+++ kcal/calendar.cpp
@@ -445,7 +445,7 @@ Incidence *Calendar::dissociateOccurrence( Incidence *incidence,
 
   Incidence *newInc = incidence->clone();
   newInc->recreate();
-  newInc->setRelatedTo( incidence );
+  //newInc->setRelatedTo( incidence );
   Recurrence *recur = newInc->recurrence();
   if ( single ) {
     recur->clear();
