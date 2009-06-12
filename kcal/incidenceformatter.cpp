f43e4bb983e9cf057885defc1a4ec0fee4a75ab4
Allen Winter <winter@kde.org>
2009-06-12 15:20:27 +0000

    merge r979860 | winterz | 2009-06-10 11:59:13 -0400 (Wed, 10 Jun 2009) | 4 lines
    implement some crash guards to in formatICalInviation.
      
    for kolab/issue3694
    
    svn path=/tags/kdepim/enterprise4.0.20090605.977848/kdepimlibs/; revision=980822

--- kcal/incidenceformatter.cpp
+++ kcal/incidenceformatter.cpp
@@ -605,6 +605,10 @@ static bool iamOrganizer( Incidence *incidence )
 {
   // Check if I'm the organizer for this incidence
 
+  if ( !incidence ) {
+    return false;
+  }
+
   bool iam = false;
   KEMailSettings settings;
   QStringList profiles = settings.profiles();
@@ -1782,7 +1786,7 @@ static QString formatICalInvitationHelper( QString invitation, Calendar *mCalend
   html += "</td></tr></table>";
 
   // Add the attendee list if I am the organizer
-  if ( myInc ) {
+  if ( myInc && helper->calendar() ) {
     html += invitationAttendees( helper->calendar()->incidence( inc->uid() ) );
   }
 
