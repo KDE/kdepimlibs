aa1b7aeb3cc5461c69ce4aba43bb80529d163864
Allen Winter <winter@kde.org>
2009-07-29 22:46:33 +0000

    Merged revisions 1004175 via svnmerge from https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r1004175 | winterz | 2009-07-29 10:09:30 -0400 (Wed, 29 Jul 2009) | 3 lines
      
      handle rsvp notices better.
      MERGE: e4,trunk,4.3.1
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepimlibs/; revision=1004367

--- kcal/incidenceformatter.cpp
+++ kcal/incidenceformatter.cpp
@@ -1810,8 +1810,15 @@ static QString formatICalInvitationHelper( QString invitation,
   if ( !myInc ) {
     html += "<br/>";
     html += "<i><u>";
-    if ( rsvpRec && ( inc && inc->revision() == 0 ) ) {
-      html += i18n( "Your response has already been recorded [%1]", ea->statusStr() );
+    if ( rsvpRec ) {
+      if ( inc && inc->revision() == 0 ) {
+        html += i18n( "Your response has already been recorded [%1]",
+                      ea->statusStr() );
+      }
+      if ( inc && inc->revision() > 0 ) {
+        html += i18n( "Your response to the update has already been recorded [%1]",
+                      ea->statusStr() );
+      }
       rsvpReq = false;
     } else if ( msg->method() == iTIPCancel ) {
       html += i18n( "Declined the invitation" );
@@ -1836,7 +1843,7 @@ static QString formatICalInvitationHelper( QString invitation,
     case iTIPRefresh:
     case iTIPAdd:
     {
-      if ( inc && inc->revision() > 0 && existingIncidence ) {
+      if ( !existingIncidence && !rsvpReq ) {
         if ( inc->type() == "Todo" ) {
           html += helper->makeLink( "reply", i18n( "[Record invitation into my to-do list]" ) );
         } else {
@@ -1879,7 +1886,7 @@ static QString formatICalInvitationHelper( QString invitation,
           html += tdClose;
         }
 
-        if ( !rsvpRec || ( inc && inc->revision() > 0 ) ) {
+        if ( !existingIncidence && !rsvpReq ) {
           // Delegate
           html += tdOpen;
           html += helper->makeLink( "delegate",
