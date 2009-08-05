424f42a918cbe30bcb5fbb093e3e8cae0958c86b
Thomas McGuire <mcguire@kde.org>
2009-08-05 17:38:22 +0000

    SVN_MERGE Merged revisions 1006407 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise4/kdepimlibs
    ........
      r1006407 | winterz | 2009-08-03 19:49:41 +0200 (Mon, 03 Aug 2009) | 7 lines
      
      merge SVN revision 1004591 by winterz from e35
      
      a better fix for kolab/issue3780 that deals with events to delete that cannot
      be found in the user's calendar.
      
      MERGE: trunk
    ........
    
    svn path=/trunk/KDE/kdepimlibs/; revision=1007390

--- kcal/incidenceformatter.cpp
+++ kcal/incidenceformatter.cpp
@@ -1842,9 +1842,9 @@ static QString formatICalInvitationHelper( QString invitation,
                     ea->statusStr() );
       rsvpReq = false;
     } else if ( msg->method() == iTIPCancel ) {
-      html += i18n( "Declined the invitation" );
+      html += i18n( "This invitation was declined" );
     } else if ( msg->method() == iTIPAdd ) {
-      html += i18n( "Accepted the invitation" );
+      html += i18n( "This invitation was accepted" );
     } else {
       html += rsvpRequestedStr( rsvpReq );
     }
