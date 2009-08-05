68028633fdfce5a8e91472bf4badc920c13aa789
Thomas McGuire <mcguire@kde.org>
2009-08-05 17:36:49 +0000

    SVN_MERGE Merged revisions 1006338 via svnmerge from svn+ssh://tmcguire@svn.kde.org/home/kde/branches/kdepim/enterprise4/kdepimlibs
    ................
      r1006338 | winterz | 2009-08-03 16:13:16 +0200 (Mon, 03 Aug 2009) | 12 lines
      
      Merged revisions 1006335 via svnmerge from 
      https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
      
      ........
        r1006335 | winterz | 2009-08-03 10:06:46 -0400 (Mon, 03 Aug 2009) | 5 lines
        
        Revert parts of SVN commit 1004175 which caused regressions in invitation handling.
        Should fix critical kolab issues 3685 and 3789
        
        MERGE: e4,trunk,4.3.1
      ........
    ................
    
    svn path=/trunk/KDE/kdepimlibs/; revision=1007388

--- kcal/incidenceformatter.cpp
+++ kcal/incidenceformatter.cpp
@@ -1864,7 +1864,7 @@ static QString formatICalInvitationHelper( QString invitation,
     case iTIPRefresh:
     case iTIPAdd:
     {
-      if ( !existingIncidence && !rsvpReq ) {
+      if ( inc && inc->revision() > 0 && existingIncidence ) {
         if ( inc->type() == "Todo" ) {
           html += helper->makeLink( "reply", i18n( "[Record invitation into my to-do list]" ) );
         } else {
@@ -1907,7 +1907,7 @@ static QString formatICalInvitationHelper( QString invitation,
           html += tdClose;
         }
 
-        if ( !existingIncidence && !rsvpReq ) {
+        if ( !rsvpRec || ( inc && inc->revision() > 0 ) ) {
           // Delegate
           html += tdOpen;
           html += helper->makeLink( "delegate",
