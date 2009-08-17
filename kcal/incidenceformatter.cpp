d9c2e00fcc4685805114a6e4051d5da8304256c8
Allen Winter <winter@kde.org>
2009-08-17 22:09:53 +0000

    Merged revisions 1012618 via svnmerge from https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    ........
      r1012618 | winterz | 2009-08-17 17:55:24 -0400 (Mon, 17 Aug 2009) | 4 lines
      
      deal appropriately with UTF8 filenames and link invitation attachments.
      kolab/issue3717
      MERGE: e4,trunk
    ........
    
    svn path=/branches/kdepim/enterprise4/kdepimlibs/; revision=1012629

--- kcal/incidenceformatter.cpp
+++ kcal/incidenceformatter.cpp
@@ -1535,7 +1535,8 @@ static QString invitationAttachments( InvitationFormatterHelper *helper, Inciden
       if ( !iconPath.isEmpty() ) {
         tmpStr += "<img src=\"" + iconPath + "\" align=\"top\">";
       }
-      tmpStr += helper->makeLink( "ATTACH:" + a->label(), a->label() );
+      tmpStr += helper->makeLink( "ATTACH:" + a->label(),
+                                  QString::fromUtf8( a->label().toLatin1() ) );
       tmpStr += "</li>";
     }
     tmpStr += "</ol>";
@@ -1753,12 +1754,14 @@ class IncidenceFormatter::IncidenceCompareVisitor
 QString InvitationFormatterHelper::makeLink( const QString &id, const QString &text )
 {
   if ( !id.startsWith( QLatin1String( "ATTACH:" ) ) ) {
-    QString res( "<a href=\"%1\"><b>%2</b></a>" );
-    return res.arg( generateLinkURL( id ) ).arg( text );
+    QString res = QString( "<a href=\"%1\"><b>%2</b></a>" ).
+                  arg( generateLinkURL( id ), text );
+    return res;
   } else {
     // draw the attachment links in non-bold face
-    QString res( "<a href=\"%1\">%2</a>" );
-    return res.arg( generateLinkURL( id ) ).arg( text );
+    QString res = QString( "<a href=\"%1\">%2</a>" ).
+                  arg( generateLinkURL( id ), text );
+    return res;
   }
 }
 
