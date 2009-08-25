5b5bb97e007a0823c782cfa7f8fe5cecebaa3282
Allen Winter <winter@kde.org>
2009-08-25 15:02:57 +0000

    merge SVN commit 1015463 by winterz from e4 to trunk: Thomas, please record this CCMAIL: thomas@kdab.net
    Merged revisions 1015459 via svnmerge from 
    https://svn.kde.org/home/kde/branches/kdepim/enterprise/kdepim
    
    ........
      r1015459 | winterz | 2009-08-25 10:47:30 -0400 (Tue, 25 Aug 2009) | 6 lines
      
      add colons to the event viewer headers ("Date","Time","Location","Organizer","Attendees")
      fixed kolab/issue3816
      
      MERGE: e4,trunk
      DO_NOT_BACKPORT: 4.3
    ........
    
    svn path=/trunk/KDE/kdepimlibs/; revision=1015467

--- kcal/incidenceformatter.cpp
+++ kcal/incidenceformatter.cpp
@@ -179,14 +179,14 @@ static QString eventViewerFormatAttendees( Incidence *event )
     const QString iconPath = iconLoader->iconPath( "mail-message-new", KIconLoader::Small );
 
     // Add organizer link
-    tmpStr += eventViewerAddTag( "h4", i18n( "Organizer" ) );
+    tmpStr += eventViewerAddTag( "h4", i18n( "Organizer:" ) );
     tmpStr += "<ul>";
     tmpStr += linkPerson( event->organizer().email(), event->organizer().name(),
                           QString(), iconPath );
     tmpStr += "</ul>";
 
     // Add attendees links
-    tmpStr += eventViewerAddTag( "h4", i18n( "Attendees" ) );
+    tmpStr += eventViewerAddTag( "h4", i18n( "Attendees:" ) );
     tmpStr += "<ul>";
     Attendee::List::ConstIterator it;
     for ( it = attendees.constBegin(); it != attendees.constEnd(); ++it ) {
@@ -324,7 +324,7 @@ static QString eventViewerFormatEvent( Event *event, KDateTime::Spec spec )
   tmpStr += "<table>";
   if ( !event->location().isEmpty() ) {
     tmpStr += "<tr>";
-    tmpStr += "<td align=\"right\"><b>" + i18n( "Location" ) + "</b></td>";
+    tmpStr += "<td align=\"right\"><b>" + i18n( "Location:" ) + "</b></td>";
     tmpStr += "<td>" + event->richLocation() + "</td>";
     tmpStr += "</tr>";
   }
@@ -332,14 +332,14 @@ static QString eventViewerFormatEvent( Event *event, KDateTime::Spec spec )
   tmpStr += "<tr>";
   if ( event->allDay() ) {
     if ( event->isMultiDay() ) {
-      tmpStr += "<td align=\"right\"><b>" + i18n( "Time" ) + "</b></td>";
+      tmpStr += "<td align=\"right\"><b>" + i18n( "Time:" ) + "</b></td>";
       tmpStr += "<td>" +
                 i18nc( "<beginTime> - <endTime>","%1 - %2",
                        IncidenceFormatter::dateToString( event->dtStart(), false, spec ),
                        IncidenceFormatter::dateToString( event->dtEnd(), false, spec ) ) +
                 "</td>";
     } else {
-      tmpStr += "<td align=\"right\"><b>" + i18n( "Date" ) + "</b></td>";
+      tmpStr += "<td align=\"right\"><b>" + i18n( "Date:" ) + "</b></td>";
       tmpStr += "<td>" +
                 i18nc( "date as string","%1",
                        IncidenceFormatter::dateToString( event->dtStart(), false, spec ) ) +
@@ -347,14 +347,14 @@ static QString eventViewerFormatEvent( Event *event, KDateTime::Spec spec )
     }
   } else {
     if ( event->isMultiDay() ) {
-      tmpStr += "<td align=\"right\"><b>" + i18n( "Time" ) + "</b></td>";
+      tmpStr += "<td align=\"right\"><b>" + i18n( "Time:" ) + "</b></td>";
       tmpStr += "<td>" +
                 i18nc( "<beginTime> - <endTime>","%1 - %2",
                        IncidenceFormatter::dateToString( event->dtStart(), false, spec ),
                        IncidenceFormatter::dateToString( event->dtEnd(), false, spec ) ) +
                 "</td>";
     } else {
-      tmpStr += "<td align=\"right\"><b>" + i18n( "Time" ) + "</b></td>";
+      tmpStr += "<td align=\"right\"><b>" + i18n( "Time:" ) + "</b></td>";
       if ( event->hasEndDate() && event->dtStart() != event->dtEnd() ) {
         tmpStr += "<td>" +
                   i18nc( "<beginTime> - <endTime>","%1 - %2",
@@ -367,7 +367,7 @@ static QString eventViewerFormatEvent( Event *event, KDateTime::Spec spec )
                   "</td>";
       }
       tmpStr += "</tr><tr>";
-      tmpStr += "<td align=\"right\"><b>" + i18n( "Date" ) + "</b></td>";
+      tmpStr += "<td align=\"right\"><b>" + i18n( "Date:" ) + "</b></td>";
       tmpStr += "<td>" +
                 i18nc( "date as string","%1",
                        IncidenceFormatter::dateToString( event->dtStart(), false, spec ) ) +
