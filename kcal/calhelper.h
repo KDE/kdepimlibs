bb2524a9c126496df931e63969bfca0e8930b793
Allen Winter <winter@kde.org>
2009-10-07 13:33:01 +0000

    merge SVN commit 1030953 by winterz from trunk to e4:
    add a new static method for determining if a calendar is using Groupware.
    MERGE: none (already in trunk)
    
    svn path=/branches/kdepim/enterprise4/kdepimlibs/; revision=1032284

--- kcal/calhelper.h
+++ kcal/calhelper.h
@@ -78,6 +78,16 @@ namespace CalHelper {
     or the Incidence is found but is not owned by the user.
   */
   KCAL_EXPORT Incidence *findMyCalendarIncidenceByUid( Calendar *calendar, const QString &uid );
+
+  /**
+    Determines if the Calendar is using a Groupware resource type.
+    @param calendar is a pointer to a valid Calendar object.
+
+    @return true if the Calendar is using a known Groupware resource type;
+    false otherwise.
+    @since 4.4
+  */
+  KCAL_EXPORT bool usingGroupware( Calendar *calendar );
 }
 
 }
