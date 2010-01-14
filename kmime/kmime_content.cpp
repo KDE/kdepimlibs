a241d6639f07f5e35e3d6dfd5fc84c2ba63a6cb4
Thomas McGuire <mcguire@kde.org>
2010-01-14 18:30:55 +0000

    Get rid of this completely, the mail serializer can produce such messages.
    svn path=/branches/work/akonadi-ports/kdepimlibs/; revision=1074770

--- kmime/kmime_content.cpp
+++ kmime/kmime_content.cpp
@@ -209,7 +209,6 @@ void Content::parse()
       // This is actual MIME multipart content.
     } else {
       // Parsing failed; treat this content as "text/plain".
-      kDebug() << "Failed to parse multipart. Treating as text/plain.";
       ct->setMimeType( "text/plain" );
       ct->setCharset( "US-ASCII" );
     }
