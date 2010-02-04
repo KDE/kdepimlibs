d72b65e3a26daae523e62a6f4d0c8ba4b78cb9a4
David Jarvie <djarvie@kde.org>
2010-02-04 19:55:26 +0000

    apidox
    svn path=/branches/KDE/4.4/kdepimlibs/; revision=1085284

--- mailtransport/transport.h
+++ mailtransport/transport.h
@@ -34,6 +34,12 @@ class TransportType;
   Represents the settings of a specific mail transport.
 
   To create a new empty Transport object, use TransportManager::createTransport().
+
+  Initialize an empty Transport object by calling the set...() methods defined in
+  kcfg-generated TransportBase, and in this class. Note that some transports use
+  the "host" setting to store the following values:
+   - Sendmail transport: path to the sendmail executable
+   - Akonadi transports: resource ID.
 */
 // TODO KDE5: Do something about the kcfg-generated TransportBase.
 // Currently it has the config stuff as private members, which means it is
