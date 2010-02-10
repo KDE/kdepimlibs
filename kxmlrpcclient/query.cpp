0a31b0d6be634973f4d63d54347cab6b34671787
Christian Weilbach <christian@whiletaker.homeip.net>
2010-02-10 18:32:53 +0000

    Backport fix for 194357.
    svn path=/branches/KDE/4.4/kdepimlibs/; revision=1088400

--- kxmlrpcclient/query.cpp
+++ kxmlrpcclient/query.cpp
@@ -227,7 +227,7 @@ QString Query::Private::marshal( const QVariant &arg ) const
         QString markup;
         markup += "<value><array><data>";
         while ( dataIterator.hasNext() ) {
-          markup += "<string><![CDATA[" + dataIterator.next() + "]]></string>\r\n";
+          markup += "<value><string><![CDATA[" + dataIterator.next() + "]]></string></value>\r\n";
         }
         markup += "</data></array></value>";
         return markup;
