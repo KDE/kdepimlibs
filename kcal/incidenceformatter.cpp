fe8ce3e7eaaabadf6f31fafb366a660381d39ce0
Allen Winter <winter@kde.org>
2010-05-04 15:41:08 +0000

    forward port SVN commit 1122739 by winterz:
    Print the Organizer in the displayView or tooltipView only if:
     I am not the organizer OR
     I am the organizer but there is at least 1 attendee who is not me
    kolab/issue4363
    
    svn path=/trunk/KDE/kdepimlibs/; revision=1122750

--- kcal/incidenceformatter.cpp
+++ kcal/incidenceformatter.cpp
@@ -265,14 +265,20 @@ static QString displayViewFormatAttendees( Incidence *incidence )
   const QString iconPath = iconLoader->iconPath( "mail-message-new", KIconLoader::Small );
 
   // Add organizer link
-  tmpStr += "<tr>";
-  tmpStr += "<td><b>" + i18n( "Organizer:" ) + "</b></td>";
-  tmpStr += "<td>" +
-            displayViewLinkPerson( incidence->organizer().email(),
-                                   incidence->organizer().name(),
-                                   QString(), iconPath ) +
-            "</td>";
-  tmpStr += "</tr>";
+  int attendeeCount = incidence->attendees().count();
+  if ( attendeeCount > 1 ||
+       ( !incidence->organizer().email().isEmpty() && !iamOrganizer( incidence ) ) ||
+       ( attendeeCount == 1 &&
+         incidence->organizer().email() != incidence->attendees().first()->email() ) ) {
+    tmpStr += "<tr>";
+    tmpStr += "<td><b>" + i18n( "Organizer:" ) + "</b></td>";
+    tmpStr += "<td>" +
+              displayViewLinkPerson( incidence->organizer().email(),
+                                     incidence->organizer().name(),
+                                     QString(), iconPath ) +
+              "</td>";
+    tmpStr += "</tr>";
+  }
 
   // Add "chair"
   str = displayViewFormatAttendeeRoleList( incidence, Attendee::Chair );
@@ -2886,9 +2892,16 @@ static QString tooltipFormatAttendees( Incidence *incidence )
   QString tmpStr, str;
 
   // Add organizer link
-  tmpStr += "<i>" + i18n( "Organizer:" ) + "</i>" + "&nbsp;";
-  tmpStr += tooltipPerson( incidence->organizer().email(),
-                           incidence->organizer().name() );
+  int attendeeCount = incidence->attendees().count();
+  if ( attendeeCount > 1 ||
+       ( !incidence->organizer().email().isEmpty() && !iamOrganizer( incidence ) ) ||
+       ( attendeeCount == 1 &&
+         incidence->organizer().email() != incidence->attendees().first()->email() ) ) {
+    tmpStr += "<i>" + i18n( "Organizer:" ) + "</i>" + "&nbsp;";
+    tmpStr += tooltipPerson( incidence->organizer().email(),
+                             incidence->organizer().name() );
+  }
+
   // Add "chair"
   str = tooltipFormatAttendeeRoleList( incidence, Attendee::Chair );
   if ( !str.isEmpty() ) {
