a0aaccae460f5431e10720003d84e31438760b33
Allen Winter <winter@kde.org>
2010-07-29 14:57:36 +0000

    Put a static copy of KPIMUtils::extractEmailAddressAndName() in here for use by fromFullName().  We do this in order to eliminate the KCalCore librarie's dependency on the KPIMUtils library.
    An even better solution is to move KPIMUtils::email into kdelibs.
    
    svn path=/trunk/KDE/kdepimlibs/; revision=1156703

--- kcalcore/person.cpp
+++ kcalcore/person.cpp
@@ -34,11 +34,6 @@
 */
 
 #include "person.h"
-
-#include "kpimutils/email.h"
-
-#include <kdebug.h>
-
 #include <QtCore/QRegExp>
 
 using namespace KCalCore;
@@ -60,14 +55,6 @@ class KCalCore::Person::Private
 
 Person::Person() : d( new KCalCore::Person::Private )
 {
-}
-
-/** static */
-Person::Ptr Person::fromFullName( const QString &fullName )
-{
-  QString email, name;
-  KPIMUtils::extractEmailAddressAndName( fullName, email, name );
-  return Person::Ptr( new Person( name, email ) );
 }
 
 Person::Person( const QString &name, const QString &email )
@@ -164,10 +151,9 @@ void Person::setEmail( const QString &email )
   }
 }
 
+/** static */
 bool Person::isValidEmail( const QString &email )
 {
-// PENDING(kdab) Review
-// can we replace this with a call to the email static functions in kpimutils? should we do that?
   int pos = email.lastIndexOf( "@" );
   return ( pos > 0 ) && ( email.lastIndexOf( "." ) > pos ) && ( ( email.length() - pos ) > 4 );
 }
@@ -207,3 +193,205 @@ QDataStream &KCalCore::operator>>( QDataStream &stream, Person::Ptr &person )
   return stream;
 }
 
+// The following function was lifted directly from KPIMUtils
+// in order to eliminate the dependency on that library.
+// Any changes made here should be ported there, and vice versa.
+static bool extractEmailAddressAndName( const QString &aStr, QString &mail, QString &name )
+{
+  name.clear();
+  mail.clear();
+
+  const int len = aStr.length();
+  const char cQuotes = '"';
+
+  bool bInComment = false;
+  bool bInQuotesOutsideOfEmail = false;
+  int i=0, iAd=0, iMailStart=0, iMailEnd=0;
+  QChar c;
+  unsigned int commentstack = 0;
+
+  // Find the '@' of the email address
+  // skipping all '@' inside "(...)" comments:
+  while ( i < len ) {
+    c = aStr[i];
+    if ( '(' == c ) {
+      commentstack++;
+    }
+    if ( ')' == c ) {
+      commentstack--;
+    }
+    bInComment = commentstack != 0;
+    if ( '"' == c && !bInComment ) {
+      bInQuotesOutsideOfEmail = !bInQuotesOutsideOfEmail;
+    }
+
+    if( !bInComment && !bInQuotesOutsideOfEmail ) {
+      if ( '@' == c ) {
+        iAd = i;
+        break; // found it
+      }
+    }
+    ++i;
+  }
+
+  if ( !iAd ) {
+    // We suppose the user is typing the string manually and just
+    // has not finished typing the mail address part.
+    // So we take everything that's left of the '<' as name and the rest as mail
+    for ( i = 0; len > i; ++i ) {
+      c = aStr[i];
+      if ( '<' != c ) {
+        name.append( c );
+      } else {
+        break;
+      }
+    }
+    mail = aStr.mid( i + 1 );
+    if ( mail.endsWith( '>' ) ) {
+      mail.truncate( mail.length() - 1 );
+    }
+
+  } else {
+    // Loop backwards until we find the start of the string
+    // or a ',' that is outside of a comment
+    //          and outside of quoted text before the leading '<'.
+    bInComment = false;
+    bInQuotesOutsideOfEmail = false;
+    for ( i = iAd-1; 0 <= i; --i ) {
+      c = aStr[i];
+      if ( bInComment ) {
+        if ( '(' == c ) {
+          if ( !name.isEmpty() ) {
+            name.prepend( ' ' );
+          }
+          bInComment = false;
+        } else {
+          name.prepend( c ); // all comment stuff is part of the name
+        }
+      } else if ( bInQuotesOutsideOfEmail ) {
+        if ( cQuotes == c ) {
+          bInQuotesOutsideOfEmail = false;
+        } else if ( c != '\\' ) {
+          name.prepend( c );
+        }
+      } else {
+        // found the start of this addressee ?
+        if ( ',' == c ) {
+          break;
+        }
+        // stuff is before the leading '<' ?
+        if ( iMailStart ) {
+          if ( cQuotes == c ) {
+            bInQuotesOutsideOfEmail = true; // end of quoted text found
+          } else {
+            name.prepend( c );
+          }
+        } else {
+          switch ( c.toLatin1() ) {
+          case '<':
+            iMailStart = i;
+            break;
+          case ')':
+            if ( !name.isEmpty() ) {
+              name.prepend( ' ' );
+            }
+            bInComment = true;
+            break;
+          default:
+            if ( ' ' != c ) {
+              mail.prepend( c );
+            }
+          }
+        }
+      }
+    }
+
+    name = name.simplified();
+    mail = mail.simplified();
+
+    if ( mail.isEmpty() ) {
+      return false;
+    }
+
+    mail.append( '@' );
+
+    // Loop forward until we find the end of the string
+    // or a ',' that is outside of a comment
+    //          and outside of quoted text behind the trailing '>'.
+    bInComment = false;
+    bInQuotesOutsideOfEmail = false;
+    int parenthesesNesting = 0;
+    for ( i = iAd+1; len > i; ++i ) {
+      c = aStr[i];
+      if ( bInComment ) {
+        if ( ')' == c ) {
+          if ( --parenthesesNesting == 0 ) {
+            bInComment = false;
+            if ( !name.isEmpty() ) {
+              name.append( ' ' );
+            }
+          } else {
+            // nested ")", add it
+            name.append( ')' ); // name can't be empty here
+          }
+        } else {
+          if ( '(' == c ) {
+            // nested "("
+            ++parenthesesNesting;
+          }
+          name.append( c ); // all comment stuff is part of the name
+        }
+      } else if ( bInQuotesOutsideOfEmail ) {
+        if ( cQuotes == c ) {
+          bInQuotesOutsideOfEmail = false;
+        } else if ( c != '\\' ) {
+          name.append( c );
+        }
+      } else {
+        // found the end of this addressee ?
+        if ( ',' == c ) {
+          break;
+        }
+        // stuff is behind the trailing '>' ?
+        if ( iMailEnd ){
+          if ( cQuotes == c ) {
+            bInQuotesOutsideOfEmail = true; // start of quoted text found
+          } else {
+            name.append( c );
+          }
+        } else {
+          switch ( c.toLatin1() ) {
+          case '>':
+            iMailEnd = i;
+            break;
+          case '(':
+            if ( !name.isEmpty() ) {
+              name.append( ' ' );
+            }
+            if ( ++parenthesesNesting > 0 ) {
+              bInComment = true;
+            }
+            break;
+          default:
+            if ( ' ' != c ) {
+              mail.append( c );
+            }
+          }
+        }
+      }
+    }
+  }
+
+  name = name.simplified();
+  mail = mail.simplified();
+
+  return ! ( name.isEmpty() || mail.isEmpty() );
+}
+
+/** static */
+Person::Ptr Person::fromFullName( const QString &fullName )
+{
+  QString email, name;
+  extractEmailAddressAndName( fullName, email, name );
+  return Person::Ptr( new Person( name, email ) );
+}
