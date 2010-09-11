8d886ebcf9db230f42d3cad9e4536d432f531c4d
Allen Winter <winter@kde.org>
2010-09-11 20:37:43 +0000

    use CMake's check_library_exists() to determine if the platform has a uuid library. then do conditional linking based on that result.
    Till: please test on Mac
    
    svn path=/trunk/KDE/kdepimlibs/; revision=1174293

--- kcalcore/ConfigureChecks.cmake
+++ kcalcore/ConfigureChecks.cmake
@@ -1,3 +1,7 @@
 include(CheckIncludeFiles)
+include(CheckLibraryExists)
 
 check_include_files(uuid/uuid.h HAVE_UUID_UUID_H)
+check_library_exists(uuid uuid_generate_random "" HAVE_UUID_LIBRARY)
+
+
