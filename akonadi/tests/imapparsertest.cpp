bb509e732fb95713d64ef1e0402d51f7a96d67fd
Tobias Koenig <tokoe@kde.org>
2010-09-26 13:25:17 +0000

    Merged revisions 1179520,1179533 via svnmerge from svn+ssh://tokoe@svn.kde.org/home/kde/trunk/KDE/kdepimlibs
    ........
      r1179520 | vkrause | 2010-09-25 20:44:37 +0200 (Sat, 25 Sep 2010) | 2 lines
      
      add a few more test cases for quote()
    ........
      r1179533 | vkrause | 2010-09-25 21:17:05 +0200 (Sat, 25 Sep 2010) | 2 lines
      
      add unit tests for join()
    ........
    
    svn path=/branches/KDE/4.5/kdepimlibs/; revision=1179906

--- akonadi/tests/imapparsertest.cpp
+++ akonadi/tests/imapparsertest.cpp
@@ -280,6 +280,12 @@ void ImapParserTest::testQuote_data()
   binaryEncoded += '\000';
   binaryEncoded += '"';
   QTest::newRow( "binary" ) << binaryNonEncoded << binaryEncoded;
+
+  QTest::newRow( "LF" ) << QByteArray( "\n" ) << QByteArray( "\"\\n\"" );
+  QTest::newRow( "CR" ) << QByteArray( "\r" ) << QByteArray( "\"\\r\"" );
+  QTest::newRow( "double quote" ) << QByteArray( "\"" ) << QByteArray( "\"\\\"\"" );
+  QTest::newRow( "mixed 1" ) << QByteArray( "a\nb\\c" ) << QByteArray( "\"a\\nb\\\\c\"" );
+  QTest::newRow( "mixed 2" ) << QByteArray( "\"a\rb\"" ) << QByteArray( "\"\\\"a\\rb\\\"\"" );
 }
 
 void ImapParserTest::testQuote()
@@ -546,6 +552,23 @@ void ImapParserTest::testBulkParser()
   }
 
   delete parser;
+}
+
+void ImapParserTest::testJoin_data()
+{
+  QTest::addColumn<QList<QByteArray> >( "list" );
+  QTest::addColumn<QByteArray>( "joined" );
+  QTest::newRow( "empty" ) << QList<QByteArray>() << QByteArray();
+  QTest::newRow( "one" ) << (QList<QByteArray>() << "abab") << QByteArray( "abab" );
+  QTest::newRow( "two" ) << (QList<QByteArray>() << "abab" << "cdcd") << QByteArray( "abab cdcd" );
+  QTest::newRow( "three" ) << (QList<QByteArray>() << "abab" << "cdcd" << "efef") << QByteArray( "abab cdcd efef" );
+}
+
+void ImapParserTest::testJoin()
+{
+  QFETCH( QList<QByteArray>, list );
+  QFETCH( QByteArray, joined );
+  QCOMPARE( ImapParser::join( list, " " ), joined );
 }
 
 #include "imapparsertest.moc"
