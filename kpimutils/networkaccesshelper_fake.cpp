765780a04e0285552dc0a8060559d9bedd5f761d
Kevin Funk <krf@gmx.de>
2010-11-25 15:53:24 +0000

    Minor: Fix copyright headers wrt email and year. It's 2010 already.
    svn path=/trunk/KDE/kdepimlibs/; revision=1200746

--- kpimutils/networkaccesshelper_fake.cpp
+++ kpimutils/networkaccesshelper_fake.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright (c) 2009 Kevin Funk <krf@electrostorm.net>
+    Copyright (c) 2010 Kevin Funk <kevin.funk@kdab.com>
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Library General Public License as published by
