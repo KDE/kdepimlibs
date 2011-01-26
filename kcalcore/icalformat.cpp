c5077d4304fe9229f8e04aa1c500685a7b9e50f8
Andreas Holzammer <andy@kdab.com>
2011-01-26 21:51:05 +0000

    dont produce memory leak
    svn path=/trunk/KDE/kdepimlibs/; revision=1217370

--- kcalcore/icalformat.cpp
+++ kcalcore/icalformat.cpp
@@ -285,11 +285,11 @@ QString ICalFormat::toString( const Calendar::Ptr &cal,
     if ( !tz ) {
       kError() << "bad time zone";
     } else {
-      component = icalcomponent_new_clone( icaltimezone_get_component( tz ) );
 #ifndef Q_OS_WINCE
       // Since WinCE does not provide a timezone database we
       // just omit this field and rely on the usage of standard
       // Olson names. 
+	  component = icalcomponent_new_clone( icaltimezone_get_component( tz ) );
       icalcomponent_add_component( calendar, component );
 #endif
       icaltimezone_free( tz, 1 );
