cae580d3e57f343a1a1faf9f8266b7d68c87085f
Christophe Giboudeaux <cgiboudeaux@gmx.com>
2012-02-13 16:16:22 +0100

    Goodbye, kcal

--- kcal/ConfigureChecks.cmake
+++
@@ -1,8 +0,0 @@
-include(CheckIncludeFiles)
-include(CheckSymbolExists)
-
-check_include_files(stdint.h HAVE_STDINT_H)
-check_include_files(stdlib.h HAVE_STDLIB_H)
-check_include_files(wctype.h HAVE_WCTYPE_H)
-check_include_files(getopt.h HAVE_GETOPT_H)
-check_symbol_exists(iswspace "wctype.h" HAVE_ISWSPACE)  # icalparser.c
--- kcal/Messages.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-$EXTRACTRC *.kcfg >> rc.cpp
-$XGETTEXT *.cpp -o $podir/libkcal.pot
