adb8516e17d81d0cff8c7b0bb14baf544b4f2099
Christian Mollekopf <chrigi_1@fastmail.fm>
2013-03-20 21:27:12 +0100

    Simplify++

--- kcalcore/icalformat.cpp
+++ kcalcore/icalformat.cpp
@@ -204,23 +204,8 @@ Incidence::Ptr ICalFormat::fromString( const QString &string )
   MemoryCalendar::Ptr cal( new MemoryCalendar( d->mTimeSpec ) );
   fromString( cal, string );
 
-  Incidence::Ptr ical;
-  Event::List elist = cal->events();
-  if ( elist.count() > 0 ) {
-    ical = elist.first();
-  } else {
-    Todo::List tlist = cal->todos();
-    if ( tlist.count() > 0 ) {
-      ical = tlist.first();
-    } else {
-      Journal::List jlist = cal->journals();
-      if ( jlist.count() > 0 ) {
-        ical = jlist.first();
-      }
-    }
-  }
-
-  return ical ? Incidence::Ptr( ical->clone() ) : Incidence::Ptr();
+  const Incidence::List list = cal->incidences();
+  return !list.isEmpty() ? Incidence::Ptr( list.first()->clone() ) : Incidence::Ptr();
 }
 
 QString ICalFormat::toString( const Calendar::Ptr &cal,
