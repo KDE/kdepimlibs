ae136a12f46506bfd4bf2885230b60b196232e40
Christophe Giboudeaux <cgiboudeaux@gmx.com>
2013-05-27 15:00:02 +0200

    Move the libical 1.0 check to config-kcalcore.h

--- kcalcore/config-kcalcore.h.cmake
+++ kcalcore/config-kcalcore.h.cmake
@@ -3,3 +3,6 @@
 
 /* Define to 1 if the libical version is equal or greater than 0.46 */
 #cmakedefine USE_ICAL_0_46
+
+/* Define to 1 if the libical version is equal or greater than 1.0 */
+#cmakedefine USE_ICAL_1_0
