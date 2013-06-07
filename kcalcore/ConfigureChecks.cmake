e5a8f4b7ad47c89981c0b707f90fc06f2ff7fdad
Montel Laurent <montel@kde.org>
2013-06-07 07:39:29 +0200

    Fix Bug 320839 - uuid library has different name on OpenBSD
    Apply patch from Vadim Zhukov
    Thanks
    
    FIXED-IN: 4.11
    BUG: 320839
    CCMAIL: persgray@gmail.com

--- kcalcore/ConfigureChecks.cmake
+++ kcalcore/ConfigureChecks.cmake
@@ -2,4 +2,7 @@ include(CheckIncludeFiles)
 include(CheckLibraryExists)
 
 check_include_files(uuid/uuid.h HAVE_UUID_UUID_H)
-check_library_exists(uuid uuid_generate_random "" HAVE_UUID_LIBRARY)
+find_library(UUID_LIBRARY NAMES e2fs-uuid uuid)
+if(UUID_LIBRARY)
+  check_library_exists("${UUID_LIBRARY}" uuid_generate_random "${LIB_INSTALL_DIR}" HAVE_UUID_LIBRARY)
+endif(UUID_LIBRARY)
