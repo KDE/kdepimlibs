fee59ea94d25f616b3fc44411e8ca01b3d787e5f
Sergio Martins <iamsergio@gmail.com>
2013-06-09 17:58:10 +0100

    Add datetime roles for getting the real start and end of exceptions.

--- kcalcore/incidencebase.h
+++ kcalcore/incidencebase.h
@@ -149,7 +149,11 @@ class KCALCORE_EXPORT IncidenceBase : public CustomProperties
                                     This is one place KCalCore isn't compliant with RFC2445) */
       RoleDisplayStart,        /**< Role for display purposes, represents the start boundary of an
                                     incidence. To-dos return dtDue here, for historical reasons */
-      RoleDnD                  /**< Role for determining new start and end dates after a DnD */
+      RoleDnD,                 /**< Role for determining new start and end dates after a DnD */
+      RoleInstanceStart,       /**< Role for determining the start date of an incidence but taking into account
+                                    that it might have recurring-id set. @since 4.11 **/
+      RoleInstanceEnd          /**< Role for determining the end date of an incidence but taking into account
+                                    that it might have recurring-id set. @since 4.11 **/
     };
 
     /**
