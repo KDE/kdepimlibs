e291e91ec74ad1968095fa518463f3b38fb8e517
Christian Mollekopf <chrigi_1@fastmail.fm>
2014-02-15 13:55:08 +0100

    Fixed endless loop in changerecorder with unhandled notifications.
    replayNext only peeked at the head of pendingNotifications, with the dequing happening
    in changeProcessed after the notification has been processed by the client.
    If noone handled the notifiation though, the code would automatically call replayNext again,
    dispatching the same notification.
    
    This patch puts the whole logic into one place (replayNext), and dequeues the notification
    before calling replayNext again.
    
    I noticed this because Akonadi::Observer doesn't yet implement the tag-related signals.

--- akonadi/changerecorder_p.h
+++ akonadi/changerecorder_p.h
@@ -43,7 +43,6 @@ class AKONADI_TESTS_EXPORT ChangeRecorderPrivate : public Akonadi::MonitorPrivat
     virtual void notificationsErased();
 
     virtual void slotNotify( const NotificationMessageV3::List &msgs );
-    virtual bool emitNotification(const Akonadi::NotificationMessageV3& msg);
 
     QString notificationsFileName() const;
 
