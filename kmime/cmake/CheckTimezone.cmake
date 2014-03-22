50c81bd146d1cab06d9c0252aa004abb6566c683
Christophe Giboudeaux <cgiboudeaux@gmx.com>
2014-03-22 18:44:56 +0100

    Coding Style
    GIT_SILENT

--- kmime/cmake/CheckTimezone.cmake
+++ kmime/cmake/CheckTimezone.cmake
@@ -1,9 +1,9 @@
 # Discover the type of the timezone variable,
 # set HAVE_TIMEZONE if found for config.h
 
-INCLUDE (CheckCXXSourceCompiles)
+include (CheckCXXSourceCompiles)
 
-CHECK_CXX_SOURCE_COMPILES("
+check_cxx_source_compiles("
 #include <stdlib.h>
 #include <ctype.h>
 #include <time.h>
@@ -12,9 +12,9 @@ int main() { timezone = 1; return 0;}
 " 
   HAVE_TIMEZONE)
 
-IF (NOT HAVE_TIMEZONE)
+if (NOT HAVE_TIMEZONE)
   # Then it's probably this variant, just to be sure
-  CHECK_CXX_SOURCE_COMPILES("
+  check_cxx_source_compiles("
 #include <stdlib.h>
 #include <ctype.h>
 #include <time.h>
@@ -24,7 +24,7 @@ int main() { const char *p = timezone(0,0); return 0;}
     HAVE_BSD_TIMEZONE)
 endif()
 
-CHECK_CXX_SOURCE_COMPILES("
+check_cxx_source_compiles("
 #include <stdlib.h>
 #include <ctype.h>
 #include <time.h>
@@ -32,4 +32,3 @@ CHECK_CXX_SOURCE_COMPILES("
 int main() { struct tm tm; tm.tm_gmtoff=1; return 0; }
 "
   HAVE_TM_GMTOFF)
-
