64c2fa485695239b374ea470db779a6f8b36e77e
Montel Laurent <montel@kde.org>
2014-04-13 10:49:39 +0200

    Move to toplevel

---
+++ akonadi-calendar/Messages.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+$EXTRACTRC `find . src -name \*.ui -o -name \*.kcfg` >> rc.cpp || exit 11
+$XGETTEXT `find . src -name "*.cpp" -o -name "*.h" | grep -v "/tests"` -o $podir/libakonadi-calendar.pot
+rm -f rc.cpp
