a683bdd42a439f28da1e7d0a1c575834a5b67e2e
Montel Laurent <montel@kde.org>
2014-07-15 22:44:20 +0200

    Fix compile with -DQT_NO_CAST_FROM_ASCII -DQT_NO_CAST_TO_ASCII

--- akonadi-mime/src/specialmailcollectionsrequestjob.cpp
+++ akonadi-mime/src/specialmailcollectionsrequestjob.cpp
@@ -86,7 +86,7 @@ SpecialMailCollectionsRequestJob::SpecialMailCollectionsRequestJob(QObject *pare
     QVariantMap options;
     options.insert(QLatin1String("Name"), displayNameMap.value("local-mail"));
     options.insert(QLatin1String("TopLevelIsContainer"), true);
-    options.insert(QLatin1String("Path"), QString(QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) + '/' + QLatin1String("local-mail")));
+    options.insert(QLatin1String("Path"), QString(QStandardPaths::writableLocation(QStandardPaths::GenericDataLocation) + QLatin1Char('/') + QLatin1String("local-mail")));
 
     setDefaultResourceType(QLatin1String("akonadi_maildir_resource"));
     setDefaultResourceOptions(options);
