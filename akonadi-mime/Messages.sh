cfc39cb75eec6b9c2158d97030555349f280a0bc
Pino Toscano <pino@kde.org>
2014-11-20 08:13:37 +0100

    i18n: do not extract messages from the "tests" subdir

--- akonadi-mime/Messages.sh
+++ akonadi-mime/Messages.sh
@@ -1,3 +1,3 @@
 #! /bin/sh
 $EXTRACTRC `find . src -name \*.rc` >> rc.cpp || exit 11
-$XGETTEXT `find . src -name "*.cpp" -o -name "*.h" | grep -v "/autotests"` -o $podir/libakonadi-kmime5.pot
+$XGETTEXT `find . src -name "*.cpp" -o -name "*.h" | grep -v "/autotests" | grep -v "/tests"` -o $podir/libakonadi-kmime5.pot
