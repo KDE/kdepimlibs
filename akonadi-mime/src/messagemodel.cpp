ccd994b32cc618571cdc5df7352e3c9aa0b98fe2
Sergio Martins <iamsergio@gmail.com>
2015-08-16 19:16:36 +0100

    Replace one QLatin1String that my script didn't catch at the time

--- akonadi-mime/src/messagemodel.cpp
+++ akonadi-mime/src/messagemodel.cpp
@@ -98,7 +98,7 @@ QVariant MessageModel::data(const QModelIndex &index, int role) const
     if (!collection().contentMimeTypes().contains(QStringLiteral("message/rfc822"))) {
         if (role == Qt::DisplayRole) {
             return i18nc("@label", "This model can only handle email folders. The current collection holds mimetypes: %1",
-                         collection().contentMimeTypes().join(QLatin1String(",")));
+                         collection().contentMimeTypes().join(QStringLiteral(",")));
         } else {
             return QVariant();
         }
