8e506105daefd1cd2d57170ff70d59114d255349
Montel Laurent <montel@kde.org>
2015-08-24 07:26:48 +0200

    Generate debug file

--- kioslave/src/ldap/kldap_debug.cpp
+++
@@ -1,2 +0,0 @@
-#include "kldap_debug.h"
-Q_LOGGING_CATEGORY(KLDAP_LOG, "log_kldap")
--- kioslave/src/ldap/kldap_debug.h
+++
@@ -1,7 +0,0 @@
-#ifndef KLDAP_DEBUG_H
-#define KLDAP_DEBUG_H
-
-#include <QLoggingCategory>
-Q_DECLARE_LOGGING_CATEGORY(KLDAP_LOG)
-
-#endif
