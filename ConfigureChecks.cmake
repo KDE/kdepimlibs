163437659b85431716eebbf416841796e99b1f18
Montel Laurent <montel@kde.org>
2016-06-12 11:26:31 +0200

    Remove this file

--- ConfigureChecks.cmake
+++
@@ -1,5 +0,0 @@
-include(CheckIncludeFiles)
-
-check_include_files(sys/select.h  HAVE_SYS_SELECT_H)
-check_include_files(sys/socket.h  HAVE_SYS_SOCKET_H)
-check_include_files(sys/types.h   HAVE_SYS_TYPES_H)
